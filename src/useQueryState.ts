380b1112b3ca73c89e7c935cfb41e47aa336c9f7
Francois Best <francois@francoisbest.com>
2021-02-26 21:12:15 +0100

    fix: Handle falsy values
    See #196.

--- src/useQueryState.ts
+++ src/useQueryState.ts
@@ -70,11 +70,11 @@ export function useQueryState<T = string>(
       // unnecessary renders when other query parameters change.
       // URLSearchParams is already polyfilled by Next.js
       const query = new URLSearchParams(window.location.search)
-      if (newValue) {
-        query.set(key, serialize(newValue))
-      } else {
+      if (newValue === null || newValue === undefined) {
         // Don't leave value-less keys hanging
         query.delete(key)
+      } else {
+        query.set(key, serialize(newValue))
       }
 
       // Remove fragment and query from asPath
