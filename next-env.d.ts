5516ad134ce12820a1f7ee56b2d3a492f394389a
Francois Best <francois@francoisbest.com>
2021-12-07 15:20:10 +0100

    chore: Upgrade to Next.js v12

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,10 +1,9 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
-declare namespace webpack {
-  export type Compiler = any
-  export type Plugin = any
-}
-declare module 'next/dist/compiled/webpack/webpack' {
-  export const webpack: any
+type StaticImageData = {
+  src: string
+  height: number
+  width: number
+  placeholder?: string
 }
