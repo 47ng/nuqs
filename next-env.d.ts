c2e7fe9798e7f8e6445c87d3b628d0aa6a67c42a
Francois Best <francois@francoisbest.com>
2021-12-12 18:09:24 +0100

    chore: Replace Next.js build fix by proper type ref

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,9 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-type StaticImageData = {
-  src: string
-  height: number
-  width: number
-  placeholder?: string
-}
+/// <reference types="next/image-types/global" />
