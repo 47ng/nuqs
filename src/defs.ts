83f1146ec71b65884d97e74923f808db0608cbd8
Francois Best <francois@francoisbest.com>
2022-01-30 23:01:15 +0100

    feat: Provide default value helpers

--- src/defs.ts
+++ src/defs.ts
@@ -5,38 +5,80 @@ export type Serializers<T> = {
   serialize: (value: T) => string
 }
 
-export type QueryTypeMap = {
-  string: Serializers<string>
-  integer: Serializers<number>
-  float: Serializers<number>
-  boolean: Serializers<boolean>
-  timestamp: Serializers<Date>
-  isoDateTime: Serializers<Date>
+export type SerializersWithDefaultFactory<T> = Serializers<T> & {
+  withDefault: (defaultValue: T) => Serializers<T> & {
+    readonly defaultValue: T
 }
+}
+
+export type QueryTypeMap = Readonly<{
+  string: SerializersWithDefaultFactory<string>
+  integer: SerializersWithDefaultFactory<number>
+  float: SerializersWithDefaultFactory<number>
+  boolean: SerializersWithDefaultFactory<boolean>
+  timestamp: SerializersWithDefaultFactory<Date>
+  isoDateTime: SerializersWithDefaultFactory<Date>
+}>
 
 export const queryTypes: QueryTypeMap = {
   string: {
     parse: v => v,
-    serialize: v => `${v}`
+    serialize: v => `${v}`,
+    withDefault(defaultValue) {
+      return {
+        ...this,
+        defaultValue
+      }
+    }
   },
   integer: {
     parse: v => parseInt(v),
-    serialize: v => Math.round(v).toFixed()
+    serialize: v => Math.round(v).toFixed(),
+    withDefault(defaultValue) {
+      return {
+        ...this,
+        defaultValue
+      }
+    }
   },
   float: {
     parse: v => parseFloat(v),
-    serialize: v => v.toString()
+    serialize: v => v.toString(),
+    withDefault(defaultValue) {
+      return {
+        ...this,
+        defaultValue
+      }
+    }
   },
   boolean: {
     parse: v => v === 'true',
-    serialize: (v: boolean) => (v ? 'true' : 'false')
+    serialize: v => (Boolean(v) ? 'true' : 'false'),
+    withDefault(defaultValue) {
+      return {
+        ...this,
+        defaultValue
+      }
+    }
   },
   timestamp: {
     parse: v => new Date(parseInt(v)),
-    serialize: (v: Date) => v.valueOf().toString()
+    serialize: (v: Date) => v.valueOf().toString(),
+    withDefault(defaultValue) {
+      return {
+        ...this,
+        defaultValue
+      }
+    }
   },
   isoDateTime: {
     parse: v => new Date(v),
-    serialize: (v: Date) => v.toISOString()
+    serialize: (v: Date) => v.toISOString(),
+    withDefault(defaultValue) {
+      return {
+        ...this,
+        defaultValue
+      }
+    }
   }
 }
