cf1772ec62abc2491627649b5cda7f0757e294c1
Francois Best <francois@francoisbest.com>
2022-02-04 23:05:51 +0100

    test: Add tests for basic query types

--- src/pages/useQueryState/index.tsx
+++ src/pages/useQueryState/index.tsx
@@ -1,16 +1,77 @@
 import { NextPage } from 'next'
 import React from 'react'
-import { useQueryState } from '../../../'
+import { queryTypes, useQueryState } from '../../../'
 
 const IntegrationPage: NextPage = () => {
-  const [state, setState] = useQueryState('key')
+  const [string, setString] = useQueryState('string')
+  const [int, setInt] = useQueryState('int', queryTypes.integer)
+  const [float, setFloat] = useQueryState('float', queryTypes.float)
+  const [bool, setBool] = useQueryState('bool', queryTypes.boolean)
   return (
-    <>
-      <button onClick={() => setState('a')}>Set A</button>
-      <button onClick={() => setState('b')}>Set B</button>
-      <button onClick={() => setState(null)}>Clear</button>
-      <p id="value">{state}</p>
-    </>
+    <main>
+      <h1>useQueryState</h1>
+      <section>
+        <h2>String</h2>
+        <button id="string_set_a" onClick={() => setString('a')}>
+          Set A
+        </button>
+        <button id="string_set_b" onClick={() => setString('b')}>
+          Set B
+        </button>
+        <button id="string_clear" onClick={() => setString(null)}>
+          Clear
+        </button>
+        <p id="string_value">{string}</p>
+      </section>
+      <section>
+        <h2>Integer</h2>
+        <button
+          id="int_increment"
+          onClick={() => setInt(old => (old ?? 0) + 1)}
+        >
+          Increment
+        </button>
+        <button
+          id="int_decrement"
+          onClick={() => setInt(old => (old ?? 0) - 1)}
+        >
+          Decrement
+        </button>
+        <button id="int_clear" onClick={() => setInt(null)}>
+          Clear
+        </button>
+        <p id="int_value">{int}</p>
+      </section>
+      <section>
+        <h2>Float</h2>
+        <button
+          id="float_increment"
+          onClick={() => setFloat(old => (old ?? 0) + 0.1)}
+        >
+          Increment by 0.1
+        </button>
+        <button
+          id="float_decrement"
+          onClick={() => setFloat(old => (old ?? 0) - 0.1)}
+        >
+          Decrement by 0.1
+        </button>
+        <button id="float_clear" onClick={() => setFloat(null)}>
+          Clear
+        </button>
+        <p id="float_value">{float}</p>
+      </section>
+      <section>
+        <h2>Boolean</h2>
+        <button id="bool_toggle" onClick={() => setBool(old => !old)}>
+          Toggle
+        </button>
+        <button id="bool_clear" onClick={() => setBool(null)}>
+          Clear
+        </button>
+        <p id="bool_value">{bool === null ? null : bool ? 'true' : 'false'}</p>
+      </section>
+    </main>
   )
 }
 
