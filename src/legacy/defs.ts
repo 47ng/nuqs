0e83fd7a2d8d9fe38b68010db1e951b21ce21a3a
Andrei Socaciu <andrewsocaciu@gmail.com>
2023-05-15 11:01:49 +0300

    chore: Add pages and app exports to dist folder for backwards compatibility

---
+++ src/legacy/defs.ts
@@ -0,0 +1,220 @@
+import type { Router } from 'next/router'
+
+// Next.js does not export the TransitionsOption interface,
+// but we can get it from where it's used:
+export type TransitionOptions = Parameters<Router['push']>[2]
+
+export type HistoryOptions = 'replace' | 'push'
+
+export type Nullable<T> = {
+  [K in keyof T]: T[K] | null
+}
+
+export type Serializers<T> = {
+  parse: (value: string) => T | null
+  serialize?: (value: T) => string
+}
+
+export type SerializersWithDefaultFactory<T> = Serializers<T> & {
+  withDefault: (defaultValue: T) => Serializers<T> & {
+    readonly defaultValue: T
+  }
+}
+
+export type QueryTypeMap = Readonly<{
+  string: SerializersWithDefaultFactory<string>
+  integer: SerializersWithDefaultFactory<number>
+  float: SerializersWithDefaultFactory<number>
+  boolean: SerializersWithDefaultFactory<boolean>
+
+  /**
+   * Querystring encoded as the number of milliseconds since epoch,
+   * and returned as a Date object.
+   */
+  timestamp: SerializersWithDefaultFactory<Date>
+
+  /**
+   * Querystring encoded as an ISO-8601 string (UTC),
+   * and returned as a Date object.
+   */
+  isoDateTime: SerializersWithDefaultFactory<Date>
+
+  /**
+   * String-based enums provide better type-safety for known sets of values.
+   * You will need to pass the stringEnum function a list of your enum values
+   * in order to validate the query string. Anything else will return `null`,
+   * or your default value if specified.
+   *
+   * Example:
+   * ```ts
+   * enum Direction {
+   *   up = 'UP',
+   *   down = 'DOWN',
+   *   left = 'LEFT',
+   *   right = 'RIGHT'
+   * }
+   *
+   * const [direction, setDirection] = useQueryState(
+   *   'direction',
+   *   queryTypes
+   *     .stringEnum<Direction>(Object.values(Direction))
+   *     .withDefault(Direction.up)
+   * )
+   * ```
+   *
+   * Note: the query string value will be the value of the enum, not its name
+   * (example above: `direction=UP`).
+   *
+   * @param validValues The values you want to accept
+   */
+  stringEnum<Enum extends string>(
+    validValues: Enum[]
+  ): SerializersWithDefaultFactory<Enum>
+
+  /**
+   * Encode any object shape into the querystring value as JSON.
+   * Value is URI-encoded for safety, so it may not look nice in the URL.
+   * Note: you may want to use `useQueryStates` for finer control over
+   * multiple related query keys.
+   */
+  json<T>(): SerializersWithDefaultFactory<T>
+
+  /**
+   * A comma-separated list of items.
+   * Items are URI-encoded for safety, so they may not look nice in the URL.
+   *
+   * @param itemSerializers Serializers for each individual item in the array
+   * @param separator The character to use to separate items (default ',')
+   */
+  array<ItemType>(
+    itemSerializers: Serializers<ItemType>,
+    separator?: string
+  ): SerializersWithDefaultFactory<ItemType[]>
+}>
+
+export const queryTypes: QueryTypeMap = {
+  string: {
+    parse: v => v,
+    serialize: v => `${v}`,
+    withDefault(defaultValue) {
+      return {
+        ...this,
+        defaultValue
+      }
+    }
+  },
+  integer: {
+    parse: v => parseInt(v),
+    serialize: v => Math.round(v).toFixed(),
+    withDefault(defaultValue) {
+      return {
+        ...this,
+        defaultValue
+      }
+    }
+  },
+  float: {
+    parse: v => parseFloat(v),
+    serialize: v => v.toString(),
+    withDefault(defaultValue) {
+      return {
+        ...this,
+        defaultValue
+      }
+    }
+  },
+  boolean: {
+    parse: v => v === 'true',
+    serialize: v => (Boolean(v) ? 'true' : 'false'),
+    withDefault(defaultValue) {
+      return {
+        ...this,
+        defaultValue
+      }
+    }
+  },
+  timestamp: {
+    parse: v => new Date(parseInt(v)),
+    serialize: (v: Date) => v.valueOf().toString(),
+    withDefault(defaultValue) {
+      return {
+        ...this,
+        defaultValue
+      }
+    }
+  },
+  isoDateTime: {
+    parse: v => new Date(v),
+    serialize: (v: Date) => v.toISOString(),
+    withDefault(defaultValue) {
+      return {
+        ...this,
+        defaultValue
+      }
+    }
+  },
+  stringEnum<Enum extends string>(validValues: Enum[]) {
+    return {
+      parse: (query: string) => {
+        const asEnum = query as unknown as Enum
+        if (validValues.includes(asEnum)) {
+          return asEnum
+        }
+        return null
+      },
+      serialize: (value: Enum) => value.toString(),
+      withDefault(defaultValue) {
+        return {
+          ...this,
+          defaultValue
+        }
+      }
+    }
+  },
+  json<T>() {
+    return {
+      parse: query => {
+        try {
+          return JSON.parse(decodeURIComponent(query)) as T
+        } catch {
+          return null
+        }
+      },
+      serialize: value => encodeURIComponent(JSON.stringify(value)),
+      withDefault(defaultValue) {
+        return {
+          ...this,
+          defaultValue
+        }
+      }
+    }
+  },
+  array(itemSerializers, separator = ',') {
+    return {
+      parse: query => {
+        type ItemType = NonNullable<ReturnType<typeof itemSerializers.parse>>
+        return query
+          .split(separator)
+          .map(item => decodeURIComponent(item))
+          .map(itemSerializers.parse)
+          .filter(value => value !== null && value !== undefined) as ItemType[]
+      },
+      serialize: values =>
+        values
+          .map<string>(value => {
+            if (itemSerializers.serialize) {
+              return itemSerializers.serialize(value)
+            }
+            return `${value}`
+          })
+          .map(encodeURIComponent)
+          .join(separator),
+      withDefault(defaultValue) {
+        return {
+          ...this,
+          defaultValue
+        }
+      }
+    }
+  }
+}
---
+++ src/legacy/tests/useQueryState.test-d.ts
@@ -0,0 +1,257 @@
+import { expectError, expectNotAssignable, expectType } from 'tsd'
+import { queryTypes, useQueryState } from '../../legacy/index'
+
+// By default, queries have a `string` state, nullable (when no query parameter is present)
+{
+  const [state, setState] = useQueryState('foo')
+  expectType<string | null>(state)
+  setState('bar')
+  setState(old => old?.toUpperCase() ?? null)
+  const out = await setState('bar')
+  expectType<boolean>(out)
+}
+
+// Accept only a single `history` option
+{
+  const [state, setState] = useQueryState('foo', { history: 'push' })
+  expectType<string | null>(state)
+  setState('bar')
+  setState(old => old?.toUpperCase() ?? null)
+  const out = await setState('bar')
+  expectType<boolean>(out)
+}
+
+// Supported query types
+{
+  const [state] = useQueryState('string', queryTypes.string)
+  expectType<string | null>(state)
+}
+{
+  const [state] = useQueryState('integer', queryTypes.integer)
+  expectType<number | null>(state)
+}
+{
+  const [state] = useQueryState('float', queryTypes.float)
+  expectType<number | null>(state)
+}
+{
+  const [state] = useQueryState('boolean', queryTypes.boolean)
+  expectType<boolean | null>(state)
+}
+{
+  const [state] = useQueryState('boolean', queryTypes.timestamp)
+  expectType<Date | null>(state)
+}
+{
+  const [state] = useQueryState('boolean', queryTypes.isoDateTime)
+  expectType<Date | null>(state)
+}
+
+// With default values, state is no longer nullable
+{
+  const [state] = useQueryState('string', queryTypes.string.withDefault('foo'))
+  expectType<string>(state)
+  expectNotAssignable<null>(state)
+}
+{
+  const [state] = useQueryState('integer', queryTypes.integer.withDefault(0))
+  expectType<number>(state)
+  expectNotAssignable<null>(state)
+}
+{
+  const [state] = useQueryState('float', queryTypes.float.withDefault(0))
+  expectType<number>(state)
+  expectNotAssignable<null>(state)
+}
+{
+  const [state] = useQueryState(
+    'boolean',
+    queryTypes.boolean.withDefault(false)
+  )
+  expectType<boolean>(state)
+  expectNotAssignable<null>(state)
+}
+{
+  const [state] = useQueryState(
+    'boolean',
+    queryTypes.timestamp.withDefault(new Date())
+  )
+  expectType<Date>(state)
+  expectNotAssignable<null>(state)
+}
+{
+  const [state] = useQueryState(
+    'boolean',
+    queryTypes.isoDateTime.withDefault(new Date())
+  )
+  expectType<Date>(state)
+  expectNotAssignable<null>(state)
+}
+
+// Default value can be spread in:
+{
+  const [state] = useQueryState('string', {
+    ...queryTypes.string,
+    defaultValue: 'foo'
+  })
+  expectType<string>(state)
+  expectNotAssignable<null>(state)
+}
+{
+  const [state] = useQueryState('integer', {
+    ...queryTypes.integer,
+    defaultValue: 0
+  })
+  expectType<number>(state)
+  expectNotAssignable<null>(state)
+}
+{
+  const [state] = useQueryState('float', {
+    ...queryTypes.float,
+    defaultValue: 0
+  })
+  expectType<number>(state)
+  expectNotAssignable<null>(state)
+}
+{
+  const [state] = useQueryState('boolean', {
+    ...queryTypes.boolean,
+    defaultValue: false
+  })
+  expectType<boolean>(state)
+  expectNotAssignable<null>(state)
+}
+{
+  const [state] = useQueryState('boolean', {
+    ...queryTypes.timestamp,
+    defaultValue: new Date()
+  })
+  expectType<Date>(state)
+  expectNotAssignable<null>(state)
+}
+{
+  const [state] = useQueryState('boolean', {
+    ...queryTypes.isoDateTime,
+    defaultValue: new Date()
+  })
+  expectType<Date>(state)
+  expectNotAssignable<null>(state)
+}
+
+// Custom serializers --
+{
+  const [hex] = useQueryState('foo', {
+    parse: input => parseInt(input, 16)
+  })
+  expectType<number | null>(hex)
+}
+{
+  const [num] = useQueryState('foo', {
+    parse: parseInt,
+    serialize: value => value.toString(16)
+  })
+  expectType<number | null>(num)
+
+  const [hex] = useQueryState('foo', {
+    parse: (input: string) => parseInt(input, 16),
+    serialize: value => value.toString(16)
+  })
+  expectType<number | null>(hex)
+
+  const [len] = useQueryState('length', {
+    parse: (input: string) => input.length,
+    serialize: value => Array.from({ length: value }, () => '•').join('')
+  })
+  expectType<number | null>(len)
+}
+{
+  const [hex] = useQueryState('foo', {
+    parse: input => parseInt(input, 16),
+    serialize: value => value.toString(16),
+    defaultValue: 0x2a
+  })
+  expectType<number>(hex)
+  expectNotAssignable<null>(hex)
+}
+{
+  const [hex] = useQueryState('foo', {
+    parse: input => parseInt(input, 16),
+    defaultValue: 0x2a
+  })
+  expectType<number>(hex)
+  expectNotAssignable<null>(hex)
+}
+
+// Allow setting `null` to clear the query
+{
+  const [, set] = useQueryState('foo')
+  set(null)
+  set(old => {
+    expectType<string | null>(old)
+    return null
+  })
+}
+{
+  const [, set] = useQueryState('foo', queryTypes.integer)
+  set(null)
+  set(old => {
+    expectType<number | null>(old)
+    return null
+  })
+}
+{
+  const [, set] = useQueryState('foo', queryTypes.float.withDefault(0.2))
+  set(null)
+  set(old => {
+    expectType<number>(old) // We know it's not null here
+    return null // But we can return null to clear the query
+  })
+}
+
+// Allow specifying just the default value for a string type
+{
+  useQueryState('foo', {
+    defaultValue: 'bar'
+  })
+  const [val, set] = useQueryState('foo', {
+    defaultValue: 'bar',
+    history: 'push'
+  })
+  expectType<string>(val)
+  set(null)
+  set(old => {
+    expectType<string>(old) // We know it's not null here
+    return null // But we can return null to clear the query
+  })
+  expectError(() => {
+    useQueryState('foo', {
+      defaultValue: 2 // not allowed for other types
+    })
+  })
+}
+
+// Expect errors on misuse
+{
+  expectError(() => {
+    useQueryState('foo', {
+      parse: (str: string) => str.length,
+      serialize: value => value.toUpperCase()
+    })
+  })
+}
+{
+  expectError(() => {
+    // parser not specified, defaults to string, should clash with explicit hook type
+    useQueryState<number>('foo')
+  })
+}
+
+// Set state to undefined
+{
+  const [, setFoo] = useQueryState('foo')
+  const [, setBar] = useQueryState('bar', queryTypes.string.withDefault('egg'))
+  expectError(() => setFoo(undefined))
+  expectError(() => setBar(undefined))
+  expectError(() => setFoo(() => undefined))
+  expectError(() => setBar(() => undefined))
+}
