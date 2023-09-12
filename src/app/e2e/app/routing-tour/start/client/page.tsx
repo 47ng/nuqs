6c4a5c6c57732aa5ab0d6dae4725d47176e3d2f7
Francois Best <github@francoisbest.com>
2023-09-12 22:24:08 +0200

    chore: Fix routing indexes

--- src/app/e2e/app/routing-tour/start/client/page.tsx
+++ src/app/e2e/app/routing-tour/start/client/page.tsx
@@ -6,22 +6,22 @@ export default function ServerStartPage() {
   return (
     <ul>
       <li>
-        <Link href="/app/routing-tour/a?from=start.client">
+        <Link href="/e2e/app/routing-tour/a?from=start.client">
           a (server, prefetch)
         </Link>
       </li>
       <li>
-        <Link href="/app/routing-tour/b?from=start.client" prefetch={false}>
+        <Link href="/e2e/app/routing-tour/b?from=start.client" prefetch={false}>
           b (server, no prefetch)
         </Link>
       </li>
       <li>
-        <Link href="/app/routing-tour/c?from=start.client">
+        <Link href="/e2e/app/routing-tour/c?from=start.client">
           c (client, prefetch)
         </Link>
       </li>
       <li>
-        <Link href="/app/routing-tour/d?from=start.client" prefetch={false}>
+        <Link href="/e2e/app/routing-tour/d?from=start.client" prefetch={false}>
           d (client, no prefetch)
         </Link>
       </li>
