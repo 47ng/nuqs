ed9c7823afab6baf777666074c45eb7b2dccefc6
Francois Best <github@francoisbest.com>
2023-11-09 10:44:00 +0100

    chore: Add tests for basePath

--- packages/e2e/cypress.config.ts
+++ packages/e2e/cypress.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'cypress'
 
+const basePath = process.env.BASE_PATH === '/' ? '' : process.env.BASE_PATH
+
 export default defineConfig({
   e2e: {
-    baseUrl: `http://localhost:3001`,
+    baseUrl: `http://localhost:3001${basePath}`,
     video: false,
     fixturesFolder: false,
     supportFile: false,
---
+++ packages/e2e/next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig } */
+const config = {
+  basePath: process.env.BASE_PATH
+}
+
+export default config
