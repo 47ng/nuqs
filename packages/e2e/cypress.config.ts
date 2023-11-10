7d35935957c73c32dee87173e6fa6431c283cc38
Francois Best <github@francoisbest.com>
2023-11-10 14:36:27 +0100

    chore: Fix undefined when running test locally

--- packages/e2e/cypress.config.ts
+++ packages/e2e/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress'
 
-const basePath = process.env.BASE_PATH === '/' ? '' : process.env.BASE_PATH
+const basePath =
+  process.env.BASE_PATH === '/' ? '' : process.env.BASE_PATH ?? ''
 
 export default defineConfig({
   e2e: {
