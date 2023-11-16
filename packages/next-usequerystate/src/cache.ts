c348a31c3322c3bc785727fc13884e8cb2670954
Francois Best <github@francoisbest.com>
2023-11-16 06:07:34 +0100

    feat: Add search params cache
    This simplifies the use of parsers on the server, and allows
    accessing search params in a type-safe manner in deeply
    nested server components.
    
    Big thanks to @eric-burel for the tip on how to use React's `cache` this way.

---
+++ packages/next-usequerystate/src/cache.ts
@@ -0,0 +1,36 @@
+import { cache } from 'react'
+import type { ParserBuilder } from './parsers'
+
+export type SearchParams = Record<string, string | string[] | undefined>
+
+type ExtractParserType<Parser> = Parser extends ParserBuilder<any>
+  ? ReturnType<Parser['parseServerSide']>
+  : never
+
+export function createSearchParamCache<
+  Parsers extends Record<string, ParserBuilder<any>>
+>(parsers: Parsers) {
+  type Keys = keyof Parsers
+  type ParsedSearchParams = {
+    [K in Keys]?: ExtractParserType<Parsers[K]>
+  }
+  const getCache = cache<() => ParsedSearchParams>(() => ({}))
+  function parseSearchParams(searchParams: SearchParams) {
+    const c = getCache()
+    for (const key in parsers) {
+      const parser = parsers[key]!
+      c[key as Keys] = parser.parseServerSide(searchParams[key])
+    }
+  }
+  function getSearchParam<Key extends keyof Parsers>(
+    key: Key
+  ): Required<ParsedSearchParams>[Key] {
+    const c = getCache()
+    const entry = c[key]
+    if (typeof entry === 'undefined') {
+      throw new Error('Forgot to parse searchParams?')
+    }
+    return entry
+  }
+  return { parseSearchParams, getSearchParam }
+}
