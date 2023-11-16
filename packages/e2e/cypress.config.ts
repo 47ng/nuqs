1b8110a78d1343b96a33c5d9c1b0cdb91c4544af
Francois Best <github@francoisbest.com>
2023-11-16 04:52:06 +0100

    chore: Fix lexicographic comparison

--- packages/e2e/cypress.config.ts
+++ packages/e2e/cypress.config.ts
@@ -1,18 +1,15 @@
 import { defineConfig } from 'cypress'
 import fs from 'node:fs'
-
-const pkgPath = new URL('./package.json', import.meta.url)
-const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
+import semver from 'semver'
 
 const basePath =
   process.env.BASE_PATH === '/' ? '' : process.env.BASE_PATH ?? ''
 
-const windowHistorySupport =
-  pkg.dependencies.next >= '14.0.3-canary.6'
-    ? process.env.WINDOW_HISTORY_SUPPORT === 'true'
-      ? 'true'
-      : 'false'
-    : 'undefined'
+const windowHistorySupport = supportsWHS()
+  ? process.env.WINDOW_HISTORY_SUPPORT === 'true'
+    ? 'true'
+    : 'false'
+  : 'undefined'
 
 export default defineConfig({
   e2e: {
@@ -28,3 +25,13 @@ export default defineConfig({
     }
   }
 })
+
+function supportsWHS() {
+  const pkgPath = new URL('./package.json', import.meta.url)
+  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
+  const nextVersion: string = pkg.dependencies.next
+  // Technically >= 14.0.3-canary.6, but semver can't match on prereleases
+  // and at the time of writing we're at 14.0.3-canary.10 and not going back,
+  // so this is a good enough approximation to avoid lexicographic comparisons.
+  return semver.satisfies(nextVersion, '^14.0.3')
+}
