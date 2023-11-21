a52195de0acd8229979704a7907b64959e62cd03
Francois Best <github@francoisbest.com>
2023-11-21 14:37:34 +0100

    test: Add e2e test for transitions

---
+++ packages/e2e/src/app/app/transitions/client.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { parseAsInteger, useQueryState } from 'next-usequerystate'
+import React from 'react'
+import { HydrationMarker } from '../../../components/hydration-marker'
+
+export function Client() {
+  const [isLoading, startTransition] = React.useTransition()
+  const [counter, setCounter] = useQueryState(
+    'counter',
+    parseAsInteger.withDefault(0).withOptions({ startTransition })
+  )
+  return (
+    <>
+      <HydrationMarker />
+      <p id="server-status">{isLoading ? 'loading' : 'idle'}</p>
+      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+    </>
+  )
+}
