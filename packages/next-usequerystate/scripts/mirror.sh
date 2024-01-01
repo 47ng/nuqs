09b7c606d5be63e4922ef07de1a96d0611c4c0dc
Francois Best <github@francoisbest.com>
2024-01-01 11:11:30 +0000

    fix: Mirror script

--- packages/next-usequerystate/scripts/mirror.sh
+++ packages/next-usequerystate/scripts/mirror.sh
@@ -5,6 +5,13 @@ set -e
 # Place ourselves in the package directory
 cd "$(dirname "$0")/.."
 
+# Abort if the version was not bumped by semantic release
+version=$(cat package.json | jq -r '.version')
+if [[ $version == "0.0.0-semantically-released" ]]; then
+  echo "Aborting publish to nuqs: the version was not bumped by semantic release"
+  exit 0
+fi
+
 # Login to the npm registry
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 
