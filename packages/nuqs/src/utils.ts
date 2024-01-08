7cc5c22c248988c4b24a9280aebb44b9280363e3
Francois Best <github@francoisbest.com>
2024-01-08 23:50:24 +0100

    fix: Safe parse everywhere
    - Fix actual printout (wrong interpolation)
    - Improve DX by printing which key failed to parse

--- packages/nuqs/src/utils.ts
+++ packages/nuqs/src/utils.ts
@@ -1,11 +1,21 @@
 import { warn } from './debug'
 import type { Parser } from './parsers'
 
-export function safeParse<T>(parser: Parser<T>['parse'], value: string) {
+export function safeParse<T>(
+  parser: Parser<T>['parse'],
+  value: string,
+  key?: string
+) {
   try {
     return parser(value)
   } catch (error) {
-    warn('[nuqs] Error while parsing value `%s`: %O', error)
+    warn(
+      '[nuqs] Error while parsing value `%s`: %O' +
+        (key ? ' (for key `%s`)' : ''),
+      value,
+      error,
+      key
+    )
     return null
   }
 }
