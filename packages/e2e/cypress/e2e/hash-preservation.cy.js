098f6e4bd1202fd4dece231f269cd741922a2515
Francois Best <github@francoisbest.com>
2024-01-10 13:42:01 +0100

    test: Test hash preservation in shallow updates

--- packages/e2e/cypress/e2e/hash-preservation.cy.js
+++ packages/e2e/cypress/e2e/hash-preservation.cy.js
@@ -5,6 +5,9 @@ function runTest() {
   cy.get('#string_set_a').click()
   cy.location('search').should('eq', '?string=a')
   cy.location('hash').should('eq', '#hash')
+  cy.get('#string_set_b').click()
+  cy.location('search').should('eq', '?string=b')
+  cy.location('hash').should('eq', '#hash')
   cy.get('#string_clear').click()
   cy.location('hash').should('eq', '#hash')
 }
