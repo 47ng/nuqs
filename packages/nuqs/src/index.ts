8d2ba249ee5766842a0a61ac115ca1cb6cba5176
Francois Best <github@francoisbest.com>
2024-01-15 22:54:50 +0100

    chore: Remove deprecated APIs
    BREAKING CHANGE: the following deprecated APIs have been removed:
    - `queryTypes` bag of parsers -> use individual `parseAsXYZ` parsers
      for better tree-shakeability.
    - `subscribeToQueryUpdates` helper -> since Next.js 14.0.5,
      `useSearchParams` is reactive to shallow search params updates
      in the app router. See #425.
    - Internal types and aliases

--- packages/nuqs/src/index.ts
+++ packages/nuqs/src/index.ts
@@ -1,10 +1,8 @@
 'use client'
 
 export type { HistoryOptions, Options } from './defs'
-export * from './deprecated'
 export * from './parsers'
 export { createSerializer } from './serializer'
-export { subscribeToQueryUpdates } from './sync'
 export type { QueryUpdateNotificationArgs, QueryUpdateSource } from './sync'
 export * from './useQueryState'
 export * from './useQueryStates'
