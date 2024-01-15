378b2af61bb5a222ad1992c1117e827d37187a8f
Francois Best <github@francoisbest.com>
2024-01-15 23:23:51 +0100

    chore: Fix path to server entrypoint

--- packages/nuqs/tsup.config.ts
+++ packages/nuqs/tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: {
     index: 'src/index.ts',
-    parsers: 'src/index.parsers.ts',
     server: 'src/index.server.ts'
   },
   format: ['esm'],
