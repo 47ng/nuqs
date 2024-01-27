15e27e152e106ef7fd81eddaf331d1fb29f3ff0c
Francois Best <github@francoisbest.com>
2024-01-27 21:23:35 +0100

    doc: Auto-compute bundle size

---
+++ packages/docs/src/app/(pages)/_landing/bundle-size.tsx
@@ -0,0 +1,12 @@
+import { execa } from 'execa'
+import path from 'node:path'
+import prettyBytes from 'pretty-bytes'
+
+export async function BundleSize() {
+  // run the bundle size script from the nuqs package
+  const { stdout } = await execa('size-limit', ['--json'], {
+    cwd: path.resolve(process.cwd(), '../../packages/nuqs')
+  })
+  const [{ size }] = JSON.parse(stdout)
+  return <>{prettyBytes(size)}</>
+}
