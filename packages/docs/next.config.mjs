805837b01d56657e173f127db2ddb669e312cbe0
Francois Best <github@francoisbest.com>
2024-02-03 15:09:59 +0100

    doc: Rework sections

--- packages/docs/next.config.mjs
+++ packages/docs/next.config.mjs
@@ -11,6 +11,15 @@ const withFumaMDX = createNextDocsMDX({
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/docs',
+        destination: '/docs/installation',
+        permanent: false
+      }
+    ]
+  },
   images: {
     remotePatterns: [
       {
