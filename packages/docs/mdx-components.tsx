aa1b9f4d9530345e453e36b31274d82f6191703d
Francois Best <github@francoisbest.com>
2024-02-03 01:08:13 +0100

    doc: Make common MDX components globally available

--- packages/docs/mdx-components.tsx
+++ packages/docs/mdx-components.tsx
@@ -1,9 +1,13 @@
-import type { MDXComponents } from 'mdx/types';
-import defaultComponents from 'next-docs-ui/mdx/default';
+import type { MDXComponents } from 'mdx/types'
+import { Callout } from 'next-docs-ui/components/callout'
+import defaultComponents from 'next-docs-ui/mdx/default'
+import { Suspense } from 'react'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...components,
-  };
+    Callout,
+    Suspense
+  }
 }
