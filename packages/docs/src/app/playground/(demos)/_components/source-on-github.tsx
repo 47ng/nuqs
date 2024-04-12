ece4d6207115919434ff1d11676ded9d80dc9411
Francois Best <github@francoisbest.com>
2024-04-12 04:02:00 +0200

    chore: Improve resilience against missing files

--- packages/docs/src/app/playground/(demos)/_components/source-on-github.tsx
+++ packages/docs/src/app/playground/(demos)/_components/source-on-github.tsx
@@ -1,4 +1,5 @@
 import { CodeBlock } from '@/src/components/code-block'
+import * as Sentry from '@sentry/nextjs'
 import { FileCode2 } from 'lucide-react'
 import fs from 'node:fs/promises'
 
@@ -22,14 +23,33 @@ export async function SourceOnGitHub({ path }: SourceOnGitHubProps) {
           Source on GitHub
         </a>
       </div>
-      <div className="not-prose overflow-hidden">
-        <CodeBlock code={source} />
-      </div>
+      {source === null && (
+        <div className="text-sm text-red-500">
+          Unable to load source code for this demo
+        </div>
+      )}
+      {source !== null && (
+        <div className="not-prose overflow-hidden">
+          <CodeBlock code={source} />
+        </div>
+      )}
     </footer>
   )
 }
 
 function readSourceCode(demoPath: string) {
   const demoFilePath = process.cwd() + '/src/app/playground/(demos)/' + demoPath
-  return fs.readFile(demoFilePath, 'utf8')
+  return fs.readFile(demoFilePath, 'utf8').catch(error => {
+    if (error.code !== 'ENOENT') {
+      throw error
+    }
+    console.error(error)
+    Sentry.captureException(error, {
+      extra: {
+        demoPath,
+        demoFilePath
+      }
+    })
+    return null
+  })
 }
