73ce3db144e9a6b84f5db2ff33074cc388041831
Rik Brown <rik@rik.codes>
2024-05-12 16:47:20 +0100

    fix: use a symbol to avoid conflict

--- packages/nuqs/src/cache.ts
+++ packages/nuqs/src/cache.ts
@@ -4,6 +4,8 @@ import type { ParserBuilder } from './parsers'
 
 export type SearchParams = Record<string, string | string[] | undefined>
 
+const $input: unique symbol = Symbol('Input')
+
 type ExtractParserType<Parser> =
   Parser extends ParserBuilder<any>
     ? ReturnType<Parser['parseServerSide']>
@@ -16,20 +18,26 @@ export function createSearchParamsCache<
   type ParsedSearchParams = {
     [K in Keys]: ExtractParserType<Parsers[K]>
   }
+
+  type Cache = {
+    searchParams: Partial<ParsedSearchParams>
+    [$input]?: SearchParams
+  }
+
   // Why not use a good old object here ?
   // React's `cache` is bound to the render lifecycle of a page,
   // whereas a simple object would be bound to the lifecycle of the process,
   // which may be reused between requests in a serverless environment
   // (warm lambdas on Vercel or AWS).
-  const getCache = cache<
-    () => Partial<ParsedSearchParams> & { _orig?: SearchParams }
-  >(() => ({}))
+  const getCache = cache<() => Cache>(() => ({
+    searchParams: {},
+  }))
   function parse(searchParams: SearchParams) {
     const c = getCache()
 
     if (Object.isFrozen(c)) {
       // parse has already been called
-      if (searchParams === c._orig) {
+      if (searchParams === c[$input]) {
         // but we're being called with the identical object again, so we can safely return the same cached result
         // (an example of when this occurs would be if parse was called in generateMetadata as well as the page itself).
         // note that this simply checks for referential equality and will still fail if a different object with the
@@ -43,23 +51,24 @@ export function createSearchParamsCache<
 
     for (const key in parsers) {
       const parser = parsers[key]!
-      c[key] = parser.parseServerSide(searchParams[key])
+      c.searchParams[key] = parser.parseServerSide(searchParams[key])
     }
 
-    c._orig = searchParams
+    c[$input] = searchParams
+    Object.freeze(c)
 
-    return Object.freeze(c) as Readonly<ParsedSearchParams>
+    return c.searchParams as Readonly<ParsedSearchParams>
   }
   function all() {
-    const c = getCache()
-    if (Object.keys(c).length === 0) {
+    const { searchParams } = getCache()
+    if (Object.keys(searchParams).length === 0) {
       throw new Error(error(500))
     }
-    return c as Readonly<ParsedSearchParams>
+    return searchParams as Readonly<ParsedSearchParams>
   }
   function get<Key extends Keys>(key: Key): ParsedSearchParams[Key] {
-    const c = getCache()
-    const entry = c[key]
+    const { searchParams } = getCache()
+    const entry = searchParams[key]
     if (typeof entry === 'undefined') {
       throw new Error(
         error(500) +
