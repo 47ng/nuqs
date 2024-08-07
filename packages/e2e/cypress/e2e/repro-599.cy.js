fb4c41b3eee97be396aa4ce24d4b4903079d1142
Francois Best <github@francoisbest.com>
2024-08-07 23:00:01 +0100

    test: Start with less encoding to verify first parsing

--- packages/e2e/cypress/e2e/repro-599.cy.js
+++ packages/e2e/cypress/e2e/repro-599.cy.js
@@ -1,8 +1,9 @@
 /// <reference types="cypress" />
 
 it('Reproduction for issue #599', () => {
+  // Start without encoding for most characters
   cy.visit(
-    '/app/repro-599?a%20%26b%3Fc%3Dd%23e%f%2Bg%22h%27i`j%3Ck%3El(m)n*o,p.q:r;s/t=init'
+    '/app/repro-599?a %26b%3Fc%3Dd%23e%f%2Bg"h\'i`j<k>l(m)n*o,p.q:r;s/t=init'
   )
   cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
   cy.get('input').should('have.value', 'init')
