a553540b8e5cff6e588b10e28d20c1213124eabb
Francois Best <github@francoisbest.com>
2024-09-01 22:59:28 +0200

    test: Fix button locator

--- packages/e2e/cypress/e2e/useQueryStates-clear-all.cy.js
+++ packages/e2e/cypress/e2e/useQueryStates-clear-all.cy.js
@@ -3,6 +3,6 @@
 it('useQueryStates clear all', () => {
   cy.visit('/app/useQueryStates-clear-all?a=foo&b=bar')
   cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
-  cy.get('#1').click()
+  cy.get('button').click()
   cy.location('search').should('eq', '')
 })
