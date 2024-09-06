ca9971df7aef9462f0a5423728aa9bb5bbe19e49
Francois Best <github@francoisbest.com>
2024-09-06 23:42:12 +0200

    chore: Remove notification types & exports

--- packages/nuqs/src/sync.ts
+++ packages/nuqs/src/sync.ts
@@ -1,13 +1,7 @@
 import Mitt from 'mitt'
 
 export const SYNC_EVENT_KEY = Symbol('__nuqs__SYNC__')
-const NOTIFY_EVENT_KEY = Symbol('__nuqs__NOTIFY__')
 
-export type QueryUpdateSource = 'internal' | 'external'
-export type QueryUpdateNotificationArgs = {
-  search: URLSearchParams
-  source: QueryUpdateSource
-}
 export type CrossHookSyncPayload = {
   state: any
   query: string | null
@@ -15,7 +9,6 @@ export type CrossHookSyncPayload = {
 
 type EventMap = {
   [SYNC_EVENT_KEY]: URLSearchParams
-  [NOTIFY_EVENT_KEY]: QueryUpdateNotificationArgs
   [key: string]: CrossHookSyncPayload
 }
 
