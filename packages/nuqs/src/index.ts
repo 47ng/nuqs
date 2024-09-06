84b3a05a2dffaf039914535da00ec70350fdceaf
Francois Best <github@francoisbest.com>
2024-09-06 23:43:10 +0200

    chore: Remove exports

--- packages/nuqs/src/index.ts
+++ packages/nuqs/src/index.ts
@@ -3,6 +3,5 @@
 export type { HistoryOptions, Options } from './defs'
 export * from './parsers'
 export { createSerializer } from './serializer'
-export type { QueryUpdateNotificationArgs, QueryUpdateSource } from './sync'
 export * from './useQueryState'
 export * from './useQueryStates'
