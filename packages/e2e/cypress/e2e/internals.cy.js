456479f0fe8b59fbaae72f11d2cfabf989dc2664
Francois Best <github@francoisbest.com>
2024-09-23 11:50:22 +0200

    chore: Remove unused internals test
    We're no longer using internals in the v2 branch.

--- packages/e2e/cypress/e2e/internals.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe('internals', () => {
-  it('works in app router', () => {
-    cy.visit('/app/internals')
-    cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
-    cy.get('#nextJsVersion').should('have.text', Cypress.env('nextJsVersion'))
-  })
-})
