20ee4387934c2c07158e8ede7af7c630a48e4e4d
Francois Best <github@francoisbest.com>
2024-09-25 23:59:54 +0200

    feat: Add Remix adapter

--- packages/nuqs/tsup.config.ts
+++ packages/nuqs/tsup.config.ts
@@ -6,7 +6,7 @@ const commonConfig = {
   format: ['esm'],
   experimentalDts: true,
   outDir: 'dist',
-  external: ['next', 'react'],
+  external: ['next', 'react', '@remix-run/react'],
   splitting: true,
   treeshake: true
 } satisfies Options
@@ -18,6 +18,7 @@ const entrypoints = {
     'adapters/next': 'src/adapters/next.ts',
     'adapters/next/app': 'src/adapters/next/app.ts',
     'adapters/next/pages': 'src/adapters/next/pages.ts',
+    'adapters/remix': 'src/adapters/remix.ts',
     'adapters/testing': 'src/adapters/testing.ts'
   },
   server: {
