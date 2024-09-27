4502c12f81f9fd026f0fa6f4184fd353d03f648d
Francois Best <github@francoisbest.com>
2024-09-27 23:58:49 +0200

    feat: Render pretty URLs in other adapters

--- packages/nuqs/src/adapters/react-router.ts
+++ packages/nuqs/src/adapters/react-router.ts
@@ -1,14 +1,21 @@
-import { useSearchParams } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import { renderQueryString } from '../url-encoding'
 import type { AdapterOptions } from './defs'
 import { createAdapterProvider } from './internal.context'
 
 function useNuqsReactRouterAdapter() {
-  const [searchParams, setSearchParams] = useSearchParams()
+  const navigate = useNavigate()
+  const [searchParams] = useSearchParams()
   const updateUrl = (search: URLSearchParams, options: AdapterOptions) => {
-    setSearchParams(search, {
-      replace: options.history === 'replace',
-      preventScrollReset: !options.scroll
-    })
+    navigate(
+      {
+        search: renderQueryString(search)
+      },
+      {
+        replace: options.history === 'replace',
+        preventScrollReset: !options.scroll
+      }
+    )
   }
   return {
     searchParams,
