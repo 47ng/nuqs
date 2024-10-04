ceea25899c9eb306c26b0d167ceab10c4670f7f3
Francois Best <github@francoisbest.com>
2024-10-04 10:24:08 +0200

    chore: Fix inline code styles

--- packages/docs/source.config.ts
+++ packages/docs/source.config.ts
@@ -1,10 +1,19 @@
+import { rehypeCode } from 'fumadocs-core/mdx-plugins'
 import { defineConfig, defineDocs } from 'fumadocs-mdx/config'
 import remarkSmartypants from 'remark-smartypants'
 
 export default defineConfig({
   lastModifiedTime: 'git',
   mdxOptions: {
-    remarkPlugins: [remarkSmartypants]
+    remarkPlugins: [remarkSmartypants],
+    rehypePlugins: [rehypeCode],
+    rehypeCodeOptions: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark'
+      },
+      inline: 'tailing-curly-colon'
+    }
   }
 })
 
