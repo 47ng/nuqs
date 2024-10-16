d4f14cc92170875e842ca25ebb512dbf937fdeb7
Francois Best <github@francoisbest.com>
2024-10-16 09:48:49 +0200

    feat: Add custom adapters API (unstable)

---
+++ packages/nuqs/src/adapters/custom.ts
@@ -0,0 +1,11 @@
+export { renderQueryString } from '../url-encoding'
+export type {
+  AdapterInterface as unstable_AdapterInterface,
+  AdapterOptions as unstable_AdapterOptions,
+  UpdateUrlFunction as unstable_UpdateUrlFunction,
+  UseAdapterHook as unstable_UseAdapterHook
+} from './defs'
+export {
+  createAdapterProvider as unstable_createAdapterProvider,
+  type AdapterContext as unstable_AdapterContext
+} from './internal.context'
