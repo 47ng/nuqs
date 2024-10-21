2b93f423be45de232392c75a7fd26e047851a6e7
Francois Best <github@francoisbest.com>
2024-10-21 13:15:14 +0200

    test: Showcase usage of the NuqsTestingAdapter

---
+++ packages/nuqs/vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['vitest.setup.ts'],
+    include: ['**/*.test.?(c|m)[jt]s?(x)'],
+    env: {
+      IS_REACT_ACT_ENVIRONMENT: 'true'
+    }
+  }
+})
