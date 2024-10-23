464506b82296248647a153e0199a21073ed8e09e
François Best <github@francoisbest.com>
2024-10-23 13:25:10 +0200

    fix: Add stub CJS exports for old ESLint plugins (#701)
    Closes #691.

---
+++ packages/nuqs/esm-only.cjs
@@ -0,0 +1,9 @@
+// ESM only manifesto: https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c
+
+// This stub file was added to let some old ESLint plugins resolve
+// the import paths correctly, as setting exports.*.require: null in package.json didn't work.
+
+throw new Error(
+  `This package is ESM only.
+  See https://github.com/47ng/nuqs/issues/691 for more details.`
+)
