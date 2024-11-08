945f784e734e3b9245795c845284af7acb8fe636
Niklas Becker <48069565+niklasbec@users.noreply.github.com>
2024-11-08 22:37:22 +0100

    Update packages/nuqs/src/errors.ts
    Co-authored-by: François Best <github@francoisbest.com>

--- packages/nuqs/src/errors.ts
+++ packages/nuqs/src/errors.ts
@@ -3,7 +3,7 @@ import { URI_MAX_LENGTH } from './utils'
 export const errors = {
   404: 'nuqs requires an adapter to work with your framework.',
   409: 'Multiple versions of the library are loaded. This may lead to unexpected behavior. Currently using `%s`, but `%s` was about to load on top.',
-  414: `Max URI length exceeded. You are trying to store too much state in the URI. Please stay below ${URI_MAX_LENGTH} characters.`,
+  414: `Max safe URL length exceeded. Some browsers may not be able to accept this URL. Consider limiting the amount of state stored in the URL`,
   429: 'URL update rate-limited by the browser. Consider increasing `throttleMs` for key(s) `%s`. %O',
   500: "Empty search params cache. Search params can't be accessed in Layouts.",
   501: 'Search params cache already populated. Have you called `parse` twice?'
