809d933940d52bcd4fed3c3680e4b544f17e5a3f
Francois Best <github@francoisbest.com>
2024-11-14 12:45:02 +0100

    test: Add referential equality tests

---
+++ packages/nuqs/src/useQueryStates.test.tsx
@@ -0,0 +1,80 @@
+import { act, renderHook } from '@testing-library/react'
+import type { ReactNode } from 'react'
+import React from 'react'
+import { describe, expect, it } from 'vitest'
+import { NuqsTestingAdapter } from './adapters/testing'
+import { parseAsArrayOf, parseAsJson, parseAsString } from './parsers'
+import { useQueryStates } from './useQueryStates'
+
+function withSearchParams(
+  searchParams?: string | URLSearchParams | Record<string, string>
+) {
+  return (props: { children: ReactNode }) => (
+    <NuqsTestingAdapter searchParams={searchParams} {...props} />
+  )
+}
+
+describe('useQueryStates', () => {
+  const defaults = {
+    str: 'foo',
+    obj: { initial: 'state' },
+    arr: [
+      {
+        initial: 'state'
+      }
+    ]
+  }
+
+  const hook = () => {
+    return useQueryStates({
+      str: parseAsString.withDefault(defaults.str),
+      obj: parseAsJson<any>(x => x).withDefault(defaults.obj),
+      arr: parseAsArrayOf(parseAsJson<any>(x => x)).withDefault(defaults.arr)
+    })
+  }
+
+  it('should have referential equality on default values', () => {
+    const { result } = renderHook(hook, { wrapper: NuqsTestingAdapter })
+    const [state] = result.current
+    expect(state.str).toBe(defaults.str)
+    expect(state.obj).toBe(defaults.obj)
+    expect(state.arr).toBe(defaults.arr)
+    expect(state.arr[0]).toBe(defaults.arr[0])
+  })
+
+  it('should keep referential equality when resetting to defaults', () => {
+    const { result } = renderHook(hook, {
+      wrapper: withSearchParams({
+        str: 'foo',
+        obj: '{"hello":"world"}',
+        arr: '{"obj":true},{"arr":true}'
+      })
+    })
+    act(() => {
+      result.current[1](null)
+    })
+    const [state] = result.current
+    expect(state.str).toBe(defaults.str)
+    expect(state.obj).toBe(defaults.obj)
+    expect(state.arr).toBe(defaults.arr)
+    expect(state.arr[0]).toBe(defaults.arr[0])
+  })
+
+  it('should keep referential equality when unrelated keys change', () => {
+    const { result } = renderHook(hook, {
+      wrapper: withSearchParams({
+        str: 'foo',
+        obj: '{"hello":"world"}'
+        // Keep arr as default
+      })
+    })
+    const [{ obj: initialObj, arr: initialArr }] = result.current
+    act(() => {
+      result.current[1]({ str: 'bar' })
+    })
+    const [{ str, obj, arr }] = result.current
+    expect(str).toBe('bar')
+    expect(obj).toBe(initialObj)
+    expect(arr).toBe(initialArr)
+  })
+})
