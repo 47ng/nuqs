4c7d325eb2124d221ff4445d741d504186c7cef1
Francois Best <github@francoisbest.com>
2024-11-14 14:00:53 +0100

    test: Use HOC in tests

--- packages/e2e/react-router/src/components/search-input.test.tsx
+++ packages/e2e/react-router/src/components/search-input.test.tsx
@@ -1,21 +1,16 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { NuqsTestingAdapter, type UrlUpdateEvent } from 'nuqs/adapters/testing'
+import {
+  withNuqsTestingAdapter,
+  type UrlUpdateEvent
+} from 'nuqs/adapters/testing'
 import { describe, expect, it, vi } from 'vitest'
 import { SearchInput } from './search-input'
 
 describe('SearchInput', () => {
   it('should render the input with state loaded from the URL', () => {
     render(<SearchInput />, {
-      wrapper: ({ children }) => (
-        <NuqsTestingAdapter
-          searchParams={{
-            search: 'nuqs'
-          }}
-        >
-          {children}
-        </NuqsTestingAdapter>
-      )
+      wrapper: withNuqsTestingAdapter({ searchParams: { search: 'nuqs' } })
     })
     const input = screen.getByRole('search')
     expect(input).toHaveValue('nuqs')
@@ -24,11 +19,9 @@ describe('SearchInput', () => {
     const user = userEvent.setup()
     const onUrlUpdate = vi.fn<[UrlUpdateEvent]>()
     render(<SearchInput />, {
-      wrapper: ({ children }) => (
-        <NuqsTestingAdapter onUrlUpdate={onUrlUpdate} rateLimitFactor={0}>
-          {children}
-        </NuqsTestingAdapter>
-      )
+      wrapper: withNuqsTestingAdapter({
+        onUrlUpdate
+      })
     })
     const expectedState = 'Hello, world!'
     const expectedParam = 'Hello,+world!'
