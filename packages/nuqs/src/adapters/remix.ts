b0841e66dc3c1481c682f886cadd419f762d89cb
Francois Best <github@francoisbest.com>
2024-11-20 20:26:20 +0100

    feat: Add shallow routing support for Remix

--- packages/nuqs/src/adapters/remix.ts
+++ packages/nuqs/src/adapters/remix.ts
@@ -1,22 +1,40 @@
 import { useNavigate, useSearchParams } from '@remix-run/react'
+import mitt from 'mitt'
+import { startTransition, useEffect, useLayoutEffect, useState } from 'react'
 import { renderQueryString } from '../url-encoding'
 import type { AdapterOptions } from './defs'
 import { createAdapterProvider } from './internal.context'
 
+const emitter = mitt<{ update: URLSearchParams }>()
+
 function useNuqsRemixAdapter() {
   const navigate = useNavigate()
-  const [searchParams] = useSearchParams()
+  const searchParams = useOptimisticSearchParams()
   const updateUrl = (search: URLSearchParams, options: AdapterOptions) => {
-    navigate(
-      {
-        search: renderQueryString(search),
-        hash: location.hash
-      },
-      {
-        replace: options.history === 'replace',
-        preventScrollReset: !options.scroll
-      }
+    startTransition(() => {
+      emitter.emit('update', search)
+    })
+    const url = new URL(location.href)
+    url.search = renderQueryString(search)
+    // First, update the URL locally without triggering a network request,
+    // this allows keeping a reactive URL if the network is slow.
+    const updateMethod =
+      options.history === 'push' ? history.pushState : history.replaceState
+    updateMethod.call(
+      history,
+      history.state, // Maintain the history state
+      '',
+      url
     )
+    if (options.scroll) {
+      window.scrollTo(0, 0)
+    }
+    if (!options.shallow) {
+      navigate(url, {
+        replace: true,
+        preventScrollReset: true
+      })
+    }
   }
   return {
     searchParams,
@@ -25,3 +43,18 @@ function useNuqsRemixAdapter() {
 }
 
 export const NuqsAdapter = createAdapterProvider(useNuqsRemixAdapter)
+
+export function useOptimisticSearchParams() {
+  const [serverSearchParams] = useSearchParams()
+  const [searchParams, setSearchParams] = useState(serverSearchParams)
+  useEffect(() => {
+    emitter.on('update', setSearchParams)
+    return () => {
+      emitter.off('update', setSearchParams)
+    }
+  }, [])
+  useLayoutEffect(() => {
+    emitter.emit('update', serverSearchParams)
+  }, [serverSearchParams])
+  return searchParams
+}
