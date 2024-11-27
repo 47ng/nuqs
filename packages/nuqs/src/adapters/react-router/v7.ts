e467149eeb7bc7ce2fe41295fe463a000fad661e
teukuamru <teukuamrullahfaisal@gmail.com>
2024-11-27 17:44:31 +0700

    ref: adding react-router v6 and v7

---
+++ packages/nuqs/src/adapters/react-router/v7.ts
@@ -0,0 +1,26 @@
+import { useNavigate, useSearchParams } from 'react-router'
+import { renderQueryString } from '../../url-encoding'
+import type { AdapterOptions } from '../defs'
+import { createAdapterProvider } from '../internal.context'
+
+function useNuqsReactRouterV7Adapter() {
+  const navigate = useNavigate()
+  const [searchParams] = useSearchParams()
+  const updateUrl = (search: URLSearchParams, options: AdapterOptions) => {
+    navigate(
+      {
+        search: renderQueryString(search)
+      },
+      {
+        replace: options.history === 'replace',
+        preventScrollReset: !options.scroll
+      }
+    )
+  }
+  return {
+    searchParams,
+    updateUrl
+  }
+}
+
+export const NuqsAdapter = createAdapterProvider(useNuqsReactRouterV7Adapter)
