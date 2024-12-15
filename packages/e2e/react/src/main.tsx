c83e01ee039ae10bb832029b7b00391679a9d2ea
Francois Best <github@francoisbest.com>
2024-12-15 20:06:21 +0100

    test: Add linking tests to React

--- packages/e2e/react/src/main.tsx
+++ packages/e2e/react/src/main.tsx
@@ -1,14 +1,15 @@
-import { HydrationMarker } from 'e2e-shared/components/hydration-marker'
 import { NuqsAdapter } from 'nuqs/adapters/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RootLayout } from './layout'
 import { Router } from './routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HydrationMarker />
     <NuqsAdapter>
-      <Router />
+      <RootLayout>
+        <Router />
+      </RootLayout>
     </NuqsAdapter>
   </StrictMode>
 )
--- packages/e2e/react/src/routes.tsx
+++ packages/e2e/react/src/routes.tsx
@@ -2,11 +2,15 @@ import { JSX, lazy } from 'react'
 
 // prettier-ignore
 const routes: Record<string, React.LazyExoticComponent<() => JSX.Element>> = {
-  '/hash-preservation':       lazy(() => import('./routes/hash-preservation')),
-  '/basic-io/useQueryState':  lazy(() => import('./routes/basic-io.useQueryState')),
-  '/basic-io/useQueryStates': lazy(() => import('./routes/basic-io.useQueryStates')),
-  '/push/useQueryState':      lazy(() => import('./routes/push.useQueryState')),
-  '/push/useQueryStates':     lazy(() => import('./routes/push.useQueryStates')),
+  '/hash-preservation':             lazy(() => import('./routes/hash-preservation')),
+  '/basic-io/useQueryState':        lazy(() => import('./routes/basic-io.useQueryState')),
+  '/basic-io/useQueryStates':       lazy(() => import('./routes/basic-io.useQueryStates')),
+  '/push/useQueryState':            lazy(() => import('./routes/push.useQueryState')),
+  '/push/useQueryStates':           lazy(() => import('./routes/push.useQueryStates')),
+  '/linking/useQueryState':         lazy(() => import('./routes/linking.useQueryState')),
+  '/linking/useQueryState/other':   lazy(() => import('./routes/linking.useQueryState.other')),
+  '/linking/useQueryStates':        lazy(() => import('./routes/linking.useQueryStates')),
+  '/linking/useQueryStates/other':  lazy(() => import('./routes/linking.useQueryStates.other')),
 }
 
 export function Router() {
