91b823b6b87394821ed01fe7e68ddf9812bb72f0
Francois Best <github@francoisbest.com>
2024-12-17 14:13:02 +0100

    doc: Deprecate the re-exported imports

--- packages/nuqs/src/adapters/react-router.ts
+++ packages/nuqs/src/adapters/react-router.ts
@@ -1,13 +1,32 @@
-/**
- * Note: this default react-router adapter is for react-router v6.
- * If you are using react-router v7, please import from `nuqs/adapters/react-router/v7`
- *
- * This shorthand import will be removed in nuqs@3.0.0.
- *
- * @deprecated Please pin your version of react-router in the import: `nuqs/adapters/react-router/v6` or `nuqs/adapters/react-router/v7`.
- */
 export {
+  /**
+   * @deprecated This import will be removed in nuqs@3.0.0.
+   *
+   * Please pin your version of React Router in the import:
+   * - `nuqs/adapters/react-router/v6`
+   * - `nuqs/adapters/react-router/v7`.
+   *
+   * Note: this deprecated import (`nuqs/adapters/react-router`) is for React Router v6 only.
+   */
   enableHistorySync,
+  /**
+   * @deprecated This import will be removed in nuqs@3.0.0.
+   *
+   * Please pin your version of React Router in the import:
+   * - `nuqs/adapters/react-router/v6`
+   * - `nuqs/adapters/react-router/v7`.
+   *
+   * Note: this deprecated import (`nuqs/adapters/react-router`) is for React Router v6 only.
+   */
   NuqsAdapter,
+  /**
+   * @deprecated This import will be removed in nuqs@3.0.0.
+   *
+   * Please pin your version of React Router in the import:
+   * - `nuqs/adapters/react-router/v6`
+   * - `nuqs/adapters/react-router/v7`.
+   *
+   * Note: this deprecated import (`nuqs/adapters/react-router`) is for React Router v6 only.
+   */
   useOptimisticSearchParams
 } from './react-router/v6'
