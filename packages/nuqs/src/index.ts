0e8108c8b08db041feacd46abe771fb3b87c019a
François Best <github@francoisbest.com>
2024-12-26 10:33:22 +0000

    feat: Add loader feature for React Routers and Remix

--- packages/nuqs/src/index.ts
+++ packages/nuqs/src/index.ts
@@ -1,4 +1,10 @@
 export type { HistoryOptions, Nullable, Options, SearchParams } from './defs'
+export {
+  createLoader,
+  type LoaderFunction,
+  type LoaderInput,
+  type LoaderOptions
+} from './loader'
 export * from './parsers'
 export { createSerializer } from './serializer'
 export * from './useQueryState'
