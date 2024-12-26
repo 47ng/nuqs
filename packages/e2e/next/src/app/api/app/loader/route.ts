d46c2123ce94f80db7d8c2e69f28bc95e3486215
François Best <github@francoisbest.com>
2024-12-26 21:11:58 +0000

    test: Try and fix test for earlier Next.js 14 versions

--- packages/e2e/next/src/app/api/app/loader/route.ts
+++ packages/e2e/next/src/app/api/app/loader/route.ts
@@ -1,6 +1,10 @@
 import { loadSearchParams } from 'e2e-shared/specs/loader'
 import { NextResponse } from 'next/server'
 
+// Needed for Next.js 14.2.0 to 14.2.3
+// (due to https://github.com/vercel/next.js/pull/66446)
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: Request) {
   const { test, int } = loadSearchParams(request)
   return new NextResponse(
