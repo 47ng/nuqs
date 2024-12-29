4e0cb0c8fbeacab8992d129bf692c285b7736c77
François Best <github@francoisbest.com>
2024-12-29 10:42:28 +0000

    test: Initial value from URL

--- packages/e2e/shared/specs/form.cy.ts
+++ packages/e2e/shared/specs/form.cy.ts
@@ -10,4 +10,16 @@ export const testForm = createTest('Form', ({ path }) => {
     cy.go('back')
     cy.get('#state').should('have.text', '')
   })
+
+  it('supports loading initial form state from the URL', () => {
+    cy.visit(path + '?test=init')
+    cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
+    cy.get('input').should('have.value', 'init')
+    cy.get('#state').should('have.text', 'init')
+    cy.get('input').clear().type('pass')
+    cy.get('form').submit()
+    cy.get('#state').should('have.text', 'pass')
+    cy.go('back')
+    cy.get('#state').should('have.text', 'init')
+  })
 })
