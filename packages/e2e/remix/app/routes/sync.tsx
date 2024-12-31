f2e605294ec597a8651bdcb47a4be0407cab9ee0
François Best <github@francoisbest.com>
2024-12-31 21:53:12 +0000

    test: Count renders with various options

--- packages/e2e/remix/app/routes/sync.tsx
+++ packages/e2e/remix/app/routes/sync.tsx
@@ -1,14 +1,19 @@
-import { useQueryState, parseAsInteger } from 'nuqs'
+import { parseAsInteger, useQueryState } from 'nuqs'
 
 function Test({ id }: { id: number }) {
   console.log(`render test ${id}`)
   const [state, setState] = useQueryState(
     'test',
-    parseAsInteger
-      .withDefault(0)
-      .withOptions({ shallow: false })
+    parseAsInteger.withDefault(0).withOptions({ shallow: false })
   )
   return <button onClick={() => setState(c => c + 1)}>{state}</button>
+}
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+export async function loader() {
+  await delay(500)
+  return null
 }
 
 export default function Page() {
