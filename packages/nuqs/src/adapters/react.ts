fad92fd8f88ba75f298a92db35a734c8adee8a09
François Best <github@francoisbest.com>
2025-01-09 20:46:34 +0100

    feat: Key isolation in the React SPA adapter
    Key isolation is the fact that updating a search param
    only re-renders hooks subscribed to this key.
    
    Most frameworks (Next.js & React Router) re-render
    every call site of their useSearchParams hooks when
    any search params change, causing unnecessary
    re-renders.
    
    Since in the React SPA we control the whole thing
    (there is no router so to speak), we can implement
    caching to detect only relevant differences and avoid
    re-rendering unnecessarily.

--- packages/nuqs/src/adapters/react.ts
+++ packages/nuqs/src/adapters/react.ts
@@ -46,7 +46,7 @@ const NuqsReactAdapterContext = createContext({
   fullPageNavigationOnShallowFalseUpdates: false
 })
 
-function useNuqsReactAdapter(): AdapterInterface {
+function useNuqsReactAdapter(watchKeys: string[]): AdapterInterface {
   const { fullPageNavigationOnShallowFalseUpdates } = useContext(
     NuqsReactAdapterContext
   )
@@ -54,21 +54,28 @@ function useNuqsReactAdapter(): AdapterInterface {
     if (typeof location === 'undefined') {
       return new URLSearchParams()
     }
-    return new URLSearchParams(location.search)
+    const search = new URLSearchParams(location.search)
+    filterSearchParams(search, watchKeys)
+    return search
   })
   useEffect(() => {
     // Popstate event is only fired when the user navigates
     // via the browser's back/forward buttons.
     const onPopState = () => {
-      setSearchParams(new URLSearchParams(location.search))
+      setSearchParams(
+        applyChange(new URLSearchParams(location.search), watchKeys)
+      )
     }
-    emitter.on('update', setSearchParams)
+    const onEmitterUpdate = (search: URLSearchParams) => {
+      setSearchParams(applyChange(search, watchKeys))
+    }
+    emitter.on('update', onEmitterUpdate)
     window.addEventListener('popstate', onPopState)
     return () => {
-      emitter.off('update', setSearchParams)
+      emitter.off('update', onEmitterUpdate)
       window.removeEventListener('popstate', onPopState)
     }
-  }, [])
+  }, [watchKeys.join('&')])
   const updateUrl = useMemo(
     () => generateUpdateUrlFn(fullPageNavigationOnShallowFalseUpdates),
     [fullPageNavigationOnShallowFalseUpdates]
@@ -104,4 +111,43 @@ export function NuqsAdapter({
  */
 export function enableHistorySync(): void {
   patchHistory(emitter, 'react')
+}
+
+function applyChange(newValue: URLSearchParams, keys: string[]) {
+  return (oldValue: URLSearchParams) => {
+    const hasChanged =
+      keys.length === 0
+        ? true
+        : keys.some(key => oldValue.get(key) !== newValue.get(key))
+    if (!hasChanged) {
+      debug(
+        '[nuqs `%s`] no change, returning previous',
+        keys.join(','),
+        oldValue
+      )
+      return oldValue
+    }
+    const copy = new URLSearchParams(newValue)
+    filterSearchParams(copy, keys)
+    debug(
+      `[nuqs \`%s\`] subbed search params change
+  from %O
+  to   %O`,
+      keys.join(','),
+      oldValue,
+      copy
+    )
+    return copy
+  }
+}
+
+function filterSearchParams(search: URLSearchParams, keys: string[]) {
+  if (keys.length === 0) {
+    return
+  }
+  for (const key of search.keys()) {
+    if (!keys.includes(key)) {
+      search.delete(key)
+    }
+  }
 }
