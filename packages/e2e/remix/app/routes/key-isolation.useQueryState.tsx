6008770ba4006977f78c1498e7e8e325331da623
François Best <github@francoisbest.com>
2025-01-10 15:41:27 +0100

    chore: Remove test loader

--- packages/e2e/remix/app/routes/key-isolation.useQueryState.tsx
+++ packages/e2e/remix/app/routes/key-isolation.useQueryState.tsx
@@ -1,10 +1,3 @@
 import { KeyIsolationUseQueryState } from 'e2e-shared/specs/key-isolation'
 
-const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
-
-export async function loader() {
-  await wait(100)
-  return null
-}
-
 export default KeyIsolationUseQueryState
