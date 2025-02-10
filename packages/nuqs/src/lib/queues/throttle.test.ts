ddbae3e78512db64f767070ad62d74faf0194129
François Best <github@francoisbest.com>
2025-02-10 21:59:34 +0100

    fix: Initial value for throttle queue timeMs
    Also renamed the property to avoid clashes with the throttleMs option.

--- packages/nuqs/src/lib/queues/throttle.test.ts
+++ packages/nuqs/src/lib/queues/throttle.test.ts
@@ -1,5 +1,7 @@
-import { describe, expect, it } from 'vitest'
-import { ThrottledQueue } from './throttle'
+import { describe, expect, it, vi } from 'vitest'
+import type { UpdateUrlFunction } from '../../adapters/lib/defs'
+import { defaultRateLimit } from './rate-limiting'
+import { ThrottledQueue, type UpdateQueueAdapterContext } from './throttle'
 
 describe('throttle: ThrottleQueue value queueing', () => {
   it('should enqueue key & values', () => {
@@ -61,5 +63,53 @@ describe('throttle: ThrottleQueue option combination logic', () => {
     queue.push({ key: 'b', query: null, options: { shallow: false } })
     queue.push({ key: 'c', query: null, options: { shallow: true } })
     expect(queue.options.shallow).toEqual(false)
+  })
+  it('should compose transitions', async () => {
+    const mockStartTransition = (callback: () => void) => {
+      callback()
+    }
+    const startTransitionA = vi.fn().mockImplementation(mockStartTransition)
+    const startTransitionB = vi.fn().mockImplementation(mockStartTransition)
+    const mockAdapter: UpdateQueueAdapterContext = {
+      updateUrl: vi.fn<UpdateUrlFunction>(),
+      getSearchParamsSnapshot() {
+        return new URLSearchParams()
+      }
+    }
+    const queue = new ThrottledQueue()
+    queue.push({
+      key: 'a',
+      query: null,
+      options: { startTransition: startTransitionA }
+    })
+    queue.push({
+      key: 'b',
+      query: null,
+      options: { startTransition: startTransitionB }
+    })
+    await queue.flush(mockAdapter)
+    expect(startTransitionA).toHaveBeenCalledOnce()
+    expect(startTransitionB).toHaveBeenCalledOnce()
+    expect(startTransitionA).toHaveBeenCalledBefore(startTransitionB)
+  })
+  it('keeps the maximum value for timeMs', () => {
+    const queue = new ThrottledQueue()
+    queue.push({ key: 'a', query: null, options: {}, timeMs: 100 })
+    queue.push({ key: 'b', query: null, options: {}, timeMs: 200 })
+    queue.push({ key: 'c', query: null, options: {}, timeMs: 300 })
+    expect(queue.timeMs).toEqual(300)
+  })
+  it('clamps the minimum value for timeMs to the default rate limit', () => {
+    expect(defaultRateLimit.timeMs).toBeGreaterThan(10) // precondition
+    const queue = new ThrottledQueue()
+    queue.push({ key: 'a', query: null, options: {}, timeMs: 10 })
+    expect(queue.timeMs).toEqual(defaultRateLimit.timeMs)
+  })
+  it('supports passing Infinity to the timeMs option (but can be cleared)', () => {
+    const queue = new ThrottledQueue()
+    queue.push({ key: 'a', query: null, options: {}, timeMs: Infinity })
+    expect(queue.timeMs).toBe(Infinity)
+    queue.push({ key: 'b', query: null, options: {}, timeMs: 100 })
+    expect(queue.timeMs).toBe(100)
   })
 })
