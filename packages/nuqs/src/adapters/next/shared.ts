1ccbe35611e57f4d9e0e9758953ba8fca5f48e53
François Best <github@francoisbest.com>
2025-02-17 13:23:02 +0100

    ref: renderURL is no longer shared

--- packages/nuqs/src/adapters/next/shared.ts
+++
@@ -1,8 +0,0 @@
-import { renderQueryString } from '../../url-encoding'
-
-export function renderURL(base: string, search: URLSearchParams) {
-  const hashlessBase = base.split('#')[0] ?? ''
-  const query = renderQueryString(search)
-  const hash = location.hash
-  return hashlessBase + query + hash
-}
