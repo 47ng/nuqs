0910b2d9f24c1eb6975c8e6d3d3d59486e8de2cd
François Best <github@francoisbest.com>
2025-03-06 10:11:54 +0100

    doc: Add React Paris banner (#931)
    * ref: Move banners
    
    * doc: Add React Paris logo
    
    * doc: Add React Paris banner

---
+++ packages/docs/src/app/banners.tsx
@@ -0,0 +1,78 @@
+import { Banner } from 'fumadocs-ui/components/banner'
+import Link from 'next/link'
+import { Suspense } from 'react'
+import { Countdown } from '../components/countdown'
+import { ReactParisLogo } from '../components/react-paris'
+
+export function NuqsV2AnnouncementTopBanner() {
+  return (
+    <Banner
+      variant="rainbow"
+      className="text-md gap-4 font-semibold"
+      id="nuqs-2-announcement"
+    >
+      <span aria-hidden>🎉</span>
+      <Link
+        href="/blog/nuqs-2"
+        className="decoration-slice decoration-1 transition-all hover:underline hover:underline-offset-8 focus-visible:underline focus-visible:outline-none"
+        prefetch={false}
+      >
+        Announcing nuqs version 2
+      </Link>
+      <span aria-hidden>🎉</span>
+    </Banner>
+  )
+}
+
+export function NuqsV2AnnouncementSidebarBanner() {
+  return (
+    <div className="my-2 flex justify-center gap-2 rounded-lg border border-blue-500/40 bg-blue-100/50 py-2.5 font-semibold dark:bg-blue-700/10">
+      <span aria-hidden>🎉</span>
+      <Link
+        href="/blog/nuqs-2"
+        className="text-blue-900 hover:underline focus-visible:underline focus-visible:outline-none dark:text-blue-100"
+        prefetch={false}
+      >
+        Announcing nuqs v2 !
+      </Link>
+      <span aria-hidden>🎉</span>
+    </div>
+  )
+}
+
+export function ReactParis2025SideBanner() {
+  return (
+    <div className="my-2 flex flex-col items-center gap-1.5 rounded-lg border border-gray-500/40 bg-gray-100/50 px-2 py-4 dark:bg-gray-700/10">
+      <p className="text-muted-foreground">🗣️ nuqs will be featured at</p>
+      <div className="flex gap-2">
+        <ReactParisLogo className="h-12" />
+        <p className="mr-1">
+          <span className="text-lg font-bold uppercase text-[#002654] dark:text-[#00acff]">
+            React
+          </span>{' '}
+          <span className="text-lg uppercase text-[#cd1126] dark:text-[#fe6497]">
+            Paris
+          </span>{' '}
+          <span className="text-lg">'25</span>
+          <br />
+          <a
+            href="https://react.paris/#tickets"
+            className="text-sm hover:underline"
+          >
+            Get your ticket now!
+          </a>
+        </p>
+      </div>
+      <Suspense>
+        <Countdown
+          targetDate={new Date('2025-03-20T15:00:00+01:00')}
+          className="my-2"
+        />
+      </Suspense>
+      <p className="text-center text-xs text-muted-foreground">
+        Use the code <code>Francois_Paris</code> for a 20% discount on your
+        ticket.
+      </p>
+    </div>
+  )
+}
---
+++ packages/docs/src/components/countdown.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import NumberFlow, { NumberFlowGroup } from '@number-flow/react'
+import { ComponentProps, ReactNode, useEffect, useState } from 'react'
+import { cn } from '../lib/utils'
+
+type CountdownProps = ComponentProps<'div'> & {
+  targetDate: Date
+  expiredMessage?: ReactNode
+}
+
+export function Countdown({
+  targetDate,
+  expiredMessage = null,
+  className,
+  ...props
+}: CountdownProps) {
+  const remaining = targetDate.getTime() - Date.now()
+  const [days, setDays] = useState(() =>
+    Math.floor(remaining / 1000 / 60 / 60 / 24)
+  )
+  const [hours, setHours] = useState(
+    () => Math.floor(remaining / 1000 / 60 / 60) % 24
+  )
+  const [minutes, setMinutes] = useState(
+    () => Math.floor(remaining / 1000 / 60) % 60
+  )
+  const [seconds, setSeconds] = useState(() =>
+    Math.floor((remaining / 1000) % 60)
+  )
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const remaining = targetDate.getTime() - Date.now()
+      setDays(Math.floor(remaining / 1000 / 60 / 60 / 24))
+      setHours(Math.floor(remaining / 1000 / 60 / 60) % 24)
+      setMinutes(Math.floor(remaining / 1000 / 60) % 60)
+      setSeconds(Math.floor((remaining / 1000) % 60))
+      if (remaining <= 0) {
+        clearInterval(timer)
+      }
+    }, 500)
+
+    return () => clearInterval(timer)
+  }, [targetDate])
+
+  if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0) {
+    return expiredMessage
+  }
+
+  return (
+    <div
+      className={cn(
+        'flex items-baseline justify-center font-bold',
+        days > 0 ? 'gap-1 text-xl' : 'text-3xl',
+        className
+      )}
+      title={targetDate.toISOString()}
+      {...props}
+    >
+      <NumberFlowGroup>
+        {days > 0 && (
+          <NumberFlow
+            trend={-1}
+            value={days}
+            className="&::part(suffix):text-gray-400 tabular-nums"
+            suffix="d"
+          />
+        )}
+        <NumberFlow
+          trend={-1}
+          value={hours}
+          className={'tabular-nums'}
+          suffix={days > 0 ? 'h' : undefined}
+          digits={{ 1: { max: 2 } }}
+          format={{ minimumIntegerDigits: 2 }}
+        />
+        <NumberFlow
+          trend={-1}
+          value={minutes}
+          className="tabular-nums"
+          prefix={days > 0 ? undefined : ':'}
+          suffix={days > 0 ? 'm' : undefined}
+          digits={{ 1: { max: 5 } }}
+          format={{ minimumIntegerDigits: 2 }}
+        />
+        <NumberFlow
+          trend={-1}
+          value={seconds}
+          className="tabular-nums"
+          prefix={days > 0 ? undefined : ':'}
+          suffix={days > 0 ? 's' : undefined}
+          digits={{ 1: { max: 5 } }}
+          format={{ minimumIntegerDigits: 2 }}
+        />
+      </NumberFlowGroup>
+    </div>
+  )
+}
