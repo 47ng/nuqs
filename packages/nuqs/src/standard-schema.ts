6eb77a7e2b4c1f209d3e349569b4890cdd01104d
François Best <github@francoisbest.com>
2025-04-06 12:03:30 +0200

    feat: Add Standard Schema interface

---
+++ packages/nuqs/src/standard-schema.ts
@@ -0,0 +1,33 @@
+import type { StandardSchemaV1 } from '@standard-schema/spec'
+import { createLoader, type CreateLoaderOptions } from './loader'
+import type { ParserMap, inferParserType } from './parsers'
+import { createSerializer } from './serializer'
+
+export function createStandardSchemaV1<Parsers extends ParserMap>(
+  parsers: Parsers,
+  { urlKeys }: CreateLoaderOptions<Parsers> = {}
+): StandardSchemaV1<unknown, inferParserType<Parsers>> {
+  const serialize = createSerializer(parsers, {})
+  const load = createLoader(parsers, { urlKeys })
+  return {
+    '~standard': {
+      version: 1,
+      vendor: 'nuqs',
+      validate(input) {
+        try {
+          const url = serialize(input as any)
+          const value = load(url, { strict: true })
+          return { value }
+        } catch (error) {
+          return {
+            issues: [
+              {
+                message: error instanceof Error ? error.message : String(error)
+              }
+            ]
+          }
+        }
+      }
+    }
+  }
+}
