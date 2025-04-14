fb6feb2ddce0b3053d2624d838f2c16b8ff67d55
François Best <github@francoisbest.com>
2025-04-14 11:10:34 +0200

    chore: Update version adoption data

--- packages/docs/src/app/(pages)/stats/searchParams.ts
+++ packages/docs/src/app/(pages)/stats/searchParams.ts
@@ -1,7 +1,7 @@
 import { createSearchParamsCache, parseAsStringLiteral } from 'nuqs/server'
 
 export const pkgOptions = ['nuqs', 'next-usequerystate', 'both'] as const
-export const pkgParser = parseAsStringLiteral(pkgOptions).withDefault('both')
+export const pkgParser = parseAsStringLiteral(pkgOptions).withDefault('nuqs')
 
 export const searchParamsCache = createSearchParamsCache({
   pkg: pkgParser
