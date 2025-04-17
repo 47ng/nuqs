38a8f26bbb52028bfe867b8bd583a429d1a2db7d
François Best <github@francoisbest.com>
2025-04-17 22:13:58 +0200

    fix: Update search cache & clear queues on popstate

--- packages/e2e/shared/specs/flush-after-navigate.cy.ts
+++ packages/e2e/shared/specs/flush-after-navigate.cy.ts
@@ -9,26 +9,36 @@ export const testFlushAfterNavigate = createTest(
     it('should not apply pending search params after navigation', () => {
       cy.visit(getUrl(path + '/start', { debounce: timeMs }))
       cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
-      cy.get('#test').click()
-      cy.get('a').click()
-      expectPathname(path + '/end')
-      cy.get('#client-state').should('be.empty')
-      cy.location('search').should('be.empty')
-      cy.wait(timeMs)
-      cy.get('#client-state').should('be.empty')
-      cy.location('search').should('be.empty')
+      function runTest() {
+        cy.get('#test').click()
+        cy.get('a').click()
+        expectPathname(path + '/end')
+        cy.get('#client-state').should('be.empty')
+        cy.location('search').should('be.empty')
+        cy.wait(timeMs)
+        cy.get('#client-state').should('be.empty')
+        cy.location('search').should('be.empty')
+      }
+      runTest()
+      cy.go('back')
+      runTest()
     })
     it('should not apply pending search params on top of existing link state when navigating to another page', () => {
       cy.visit(getUrl(path + '/start', { debounce: timeMs, linkState: 'nav' }))
       cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
-      cy.get('#test').click()
-      cy.get('a').click()
-      expectPathname(path + '/end')
-      cy.get('#client-state').should('have.text', 'nav')
-      cy.location('search').should('eq', '?test=nav')
-      cy.wait(timeMs)
-      cy.get('#client-state').should('have.text', 'nav')
-      cy.location('search').should('eq', '?test=nav')
+      function runTest() {
+        cy.get('#test').click()
+        cy.get('a').click()
+        expectPathname(path + '/end')
+        cy.get('#client-state').should('have.text', 'nav')
+        cy.location('search').should('eq', '?test=nav')
+        cy.wait(timeMs)
+        cy.get('#client-state').should('have.text', 'nav')
+        cy.location('search').should('eq', '?test=nav')
+      }
+      runTest()
+      cy.go('back')
+      runTest()
     })
     it('should not apply pending search params on top of existing link state when navigating to the same page', () => {
       cy.visit(
@@ -39,39 +49,54 @@ export const testFlushAfterNavigate = createTest(
         })
       )
       cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
-      cy.get('#test').click()
-      cy.get('a').click()
-      expectPathname(path + '/start')
-      cy.location('search').should('eq', '?test=nav')
-      cy.get('#client-state').should('have.text', 'nav')
-      cy.wait(timeMs)
-      cy.get('#client-state').should('have.text', 'nav')
-      cy.location('search').should('eq', '?test=nav')
+      function runTest() {
+        cy.get('#test').click()
+        cy.get('a').click()
+        expectPathname(path + '/start')
+        cy.location('search').should('eq', '?test=nav')
+        cy.get('#client-state').should('have.text', 'nav')
+        cy.wait(timeMs)
+        cy.get('#client-state').should('have.text', 'nav')
+        cy.location('search').should('eq', '?test=nav')
+      }
+      runTest()
+      cy.go('back')
+      runTest()
     })
     it('should not apply pending search params queued while throttled after navigation', () => {
       cy.visit(getUrl(path + '/start', { throttle: timeMs }))
       cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
-      cy.get('#preflush').click() // Trigger an immediate flush to enable the throttling queue
-      cy.get('#test').click() // Queue the change
-      cy.get('a').click() // Navigate
-      expectPathname(path + '/end')
-      cy.get('#client-state').should('be.empty')
-      cy.location('search').should('be.empty')
-      cy.wait(timeMs)
-      cy.get('#client-state').should('be.empty')
-      cy.location('search').should('be.empty')
+      function runTest() {
+        cy.get('#preflush').click() // Trigger an immediate flush to enable the throttling queue
+        cy.get('#test').click() // Queue the change
+        cy.get('a').click() // Navigate
+        expectPathname(path + '/end')
+        cy.get('#client-state').should('be.empty')
+        cy.location('search').should('be.empty')
+        cy.wait(timeMs)
+        cy.get('#client-state').should('be.empty')
+        cy.location('search').should('be.empty')
+      }
+      runTest()
+      cy.go('back')
+      runTest()
     })
     it('should not apply pending search params queued while throttled after navigation with link state', () => {
       cy.visit(getUrl(path + '/start', { throttle: timeMs, linkState: 'nav' }))
       cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
-      cy.get('#preflush').click() // Trigger an immediate flush to enable the throttling queue
-      cy.get('#test').click() // Queue the change
-      cy.get('a').click() // Navigate
-      cy.get('#client-state').should('have.text', 'nav')
-      cy.location('search').should('eq', '?test=nav')
-      cy.wait(timeMs)
-      cy.get('#client-state').should('have.text', 'nav')
-      cy.location('search').should('eq', '?test=nav')
+      function runTest() {
+        cy.get('#preflush').click() // Trigger an immediate flush to enable the throttling queue
+        cy.get('#test').click() // Queue the change
+        cy.get('a').click() // Navigate
+        cy.get('#client-state').should('have.text', 'nav')
+        cy.location('search').should('eq', '?test=nav')
+        cy.wait(timeMs)
+        cy.get('#client-state').should('have.text', 'nav')
+        cy.location('search').should('eq', '?test=nav')
+      }
+      runTest()
+      cy.go('back')
+      runTest()
     })
   }
 )
