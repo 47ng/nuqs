cc7abe00e5c9eaeac4f598482f9e73ae0e44516a
François Best <github@francoisbest.com>
2025-04-21 11:52:51 +0200

    test: Add stitching staggered updates
    Making sure updates made in different event loop ticks stitch together in the right order.

--- packages/e2e/shared/specs/stitching.tsx
+++ packages/e2e/shared/specs/stitching.tsx
@@ -10,15 +10,29 @@ export function Stitching() {
   const [b, setB] = useQueryState('b', parser)
   const [c, setC] = useQueryState('c', parser)
 
-  const test = () => {
+  const testOnSameTick = () => {
     setA(x => x + 1)
     setB(x => x + 1, { limitUrlUpdates: debounce(100) })
     setC(x => x + 1, { limitUrlUpdates: debounce(200) })
   }
+  const testStaggered = () => {
+    setC(x => x + 1, { limitUrlUpdates: debounce(200) })
+    setTimeout(() => {
+      setB(x => x + 1, { limitUrlUpdates: debounce(100) })
+      setTimeout(() => {
+        setA(x => x + 1)
+      }, 0)
+    }, 0)
+  }
 
   return (
     <>
-      <button onClick={test}>Test</button>
+      <button id="same-tick" onClick={testOnSameTick}>
+        Test on same tick
+      </button>
+      <button id="staggered" onClick={testStaggered}>
+        Test staggered
+      </button>
       <Display environment="client" target="a" state={a} />
       <Display environment="client" target="b" state={b} />
       <Display environment="client" target="c" state={c} />
