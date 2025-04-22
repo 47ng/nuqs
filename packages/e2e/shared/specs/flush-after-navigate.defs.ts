62df6dd1a63fc8bc5ee2c249e70f88019d394372
François Best <github@francoisbest.com>
2025-04-22 10:41:55 +0200

    test: Flush after navigate with shallow & history options

--- packages/e2e/shared/specs/flush-after-navigate.defs.ts
+++ packages/e2e/shared/specs/flush-after-navigate.defs.ts
@@ -1,6 +1,7 @@
 import {
   createLoader,
   createSerializer,
+  parseAsBoolean,
   parseAsInteger,
   parseAsString,
   parseAsStringLiteral
@@ -10,7 +11,13 @@ export const searchParams = {
   debounce: parseAsInteger,
   throttle: parseAsInteger,
   linkState: parseAsString,
+  history: parseAsStringLiteral(['push', 'replace']).withDefault('replace'),
+  shallow: parseAsBoolean.withDefault(true),
   linkPath: parseAsStringLiteral(['/end', '/start']).withDefault('/end')
+}
+
+export const testSearchParams = {
+  test: parseAsString
 }
 
 export const loadSearchParams = createLoader(searchParams)
