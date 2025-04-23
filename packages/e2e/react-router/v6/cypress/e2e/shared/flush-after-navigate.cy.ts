4613620892dab87bb3ed4388e5146638c288f80f
François Best <github@francoisbest.com>
2025-04-23 10:50:15 +0200

    test: Add useQueryStates testing for flush-after-navigate

--- packages/e2e/react-router/v6/cypress/e2e/shared/flush-after-navigate.cy.ts
+++ packages/e2e/react-router/v6/cypress/e2e/shared/flush-after-navigate.cy.ts
@@ -1,3 +1,11 @@
 import { testFlushAfterNavigate } from 'e2e-shared/specs/flush-after-navigate.cy'
 
-testFlushAfterNavigate({ path: '/flush-after-navigate' })
+testFlushAfterNavigate({
+  path: '/flush-after-navigate/useQueryState',
+  hook: 'useQueryState'
+})
+
+testFlushAfterNavigate({
+  path: '/flush-after-navigate/useQueryStates',
+  hook: 'useQueryStates'
+})
--- packages/e2e/shared/specs/flush-after-navigate.tsx
+++ packages/e2e/shared/specs/flush-after-navigate.tsx
@@ -7,7 +7,11 @@ import { searchParams, testSearchParams } from './flush-after-navigate.defs'
 
 const getLink = createSerializer(testSearchParams)
 
-export function FlushAfterNavigateStart({ path }: { path: string }) {
+export function FlushAfterNavigateUseQueryStateStart({
+  path
+}: {
+  path: string
+}) {
   const [{ debounce, throttle, linkState, linkPath, history, shallow }] =
     useQueryStates(searchParams)
   const Link = useLink()
@@ -38,7 +42,56 @@ export function FlushAfterNavigateStart({ path }: { path: string }) {
   )
 }
 
+export function FlushAfterNavigateUseQueryStatesStart({
+  path
+}: {
+  path: string
+}) {
+  const [{ debounce, throttle, linkState, linkPath, history, shallow }] =
+    useQueryStates(searchParams)
+  const Link = useLink()
+  const [, preflush] = useQueryState('preflush')
+  const [{ test: state }, setState] = useQueryStates(testSearchParams, {
+    history,
+    shallow,
+    limitUrlUpdates:
+      debounce !== null
+        ? { method: 'debounce', timeMs: debounce }
+        : throttle !== null
+          ? { method: 'throttle', timeMs: throttle }
+          : undefined
+  })
+  return (
+    <>
+      <button id="test" onClick={() => setState({ test: 'fail' })}>
+        Test
+      </button>
+      <button id="preflush" onClick={() => preflush(null)}>
+        Preflush
+      </button>
+      <Link href={getLink(`${path}${linkPath}`, { test: linkState })}>
+        Link
+      </Link>
+      <Display environment="client" state={state} />
+    </>
+  )
+}
+
 export function FlushAfterNavigateEnd() {
-  const [state] = useQueryState('test')
-  return <Display environment="client" state={state} />
+  const [stateUseQueryState] = useQueryState('test')
+  const [{ test: stateUseQueryStates }] = useQueryStates(testSearchParams)
+  return (
+    <>
+      <Display
+        environment="client"
+        target="useQueryState"
+        state={stateUseQueryState}
+      />
+      <Display
+        environment="client"
+        target="useQueryStates"
+        state={stateUseQueryStates}
+      />
+    </>
+  )
 }
