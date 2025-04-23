7e5dc0bb5ceb8d66424405ea2ab9fbfe2e5af937
François Best <github@francoisbest.com>
2025-04-23 11:04:09 +0200

    chore: Retry all stitching tests locally

--- packages/e2e/shared/specs/stitching.cy.ts
+++ packages/e2e/shared/specs/stitching.cy.ts
@@ -28,29 +28,35 @@ export function testStitching({
               )
             }
 
-            it('should update the state optimistically and sequence the URL updates', () => {
-              cy.visit(getUrl(path, { hook, shallow, history }))
-              cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
-              cy.get('#same-tick').click()
-              cy.get('#client-state').should('have.text', '1,1,1')
-              assertSearch('a=1')
-              assertSearch('a=1&b=1')
-              assertSearch('a=1&b=1&c=1')
-              cy.get('#same-tick').click()
-              cy.get('#client-state').should('have.text', '2,2,2')
-              assertSearch('a=2&b=1&c=1')
-              assertSearch('a=2&b=2&c=1')
-              assertSearch('a=2&b=2&c=2')
-              cy.get('#same-tick').click()
-              cy.get('#client-state').should('have.text', '3,3,3')
-              assertSearch('a=3&b=2&c=2')
-              // Don't wait till completion, queue an update before the debounced resolve
-              cy.get('#same-tick').click()
-              cy.get('#client-state').should('have.text', '4,4,4')
-              assertSearch('a=4&b=2&c=2')
-              assertSearch('a=4&b=4&c=2')
-              assertSearch('a=4&b=4&c=4')
-            })
+            it(
+              'should update the state optimistically and sequence the URL updates',
+              {
+                ...(Cypress.env('CI') ? undefined : { retries: 2 })
+              },
+              () => {
+                cy.visit(getUrl(path, { hook, shallow, history }))
+                cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
+                cy.get('#same-tick').click()
+                cy.get('#client-state').should('have.text', '1,1,1')
+                assertSearch('a=1')
+                assertSearch('a=1&b=1')
+                assertSearch('a=1&b=1&c=1')
+                cy.get('#same-tick').click()
+                cy.get('#client-state').should('have.text', '2,2,2')
+                assertSearch('a=2&b=1&c=1')
+                assertSearch('a=2&b=2&c=1')
+                assertSearch('a=2&b=2&c=2')
+                cy.get('#same-tick').click()
+                cy.get('#client-state').should('have.text', '3,3,3')
+                assertSearch('a=3&b=2&c=2')
+                // Don't wait till completion, queue an update before the debounced resolve
+                cy.get('#same-tick').click()
+                cy.get('#client-state').should('have.text', '4,4,4')
+                assertSearch('a=4&b=2&c=2')
+                assertSearch('a=4&b=4&c=2')
+                assertSearch('a=4&b=4&c=4')
+              }
+            )
 
             it(
               'should sequence updates when staggered',
