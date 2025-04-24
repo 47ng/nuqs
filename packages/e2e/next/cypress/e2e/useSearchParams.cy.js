498ffcc72c108d7574df873d8743bdcda3198421
François Best <github@francoisbest.com>
2025-04-25 00:14:10 +0200

    test: Combine repros & remove useless Next.js version check

--- packages/e2e/next/cypress/e2e/useSearchParams.cy.js
+++ packages/e2e/next/cypress/e2e/useSearchParams.cy.js
@@ -1,12 +1,10 @@
 /// <reference types="cypress" />
 
-if (Cypress.env('supportsShallowRouting')) {
-  it('useSearchParams', () => {
-    cy.visit('/app/useSearchParams')
-    cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
-    cy.get('input').type('foo', { delay: 0 })
-    cy.get('#searchParams').should('have.text', 'q=foo')
-    cy.get('button').click()
-    cy.get('#searchParams').should('have.text', 'q=foo&push=true')
-  })
-}
+it('useSearchParams', () => {
+  cy.visit('/app/useSearchParams')
+  cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
+  cy.get('input').type('foo', { delay: 0 })
+  cy.get('#searchParams').should('have.text', 'q=foo')
+  cy.get('button').click()
+  cy.get('#searchParams').should('have.text', 'q=foo&push=true')
+})
