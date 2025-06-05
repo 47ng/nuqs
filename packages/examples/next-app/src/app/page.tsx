f1176194e063dcfda051817d9a22c90a3cb3267e
François Best <github@francoisbest.com>
2025-06-05 23:05:29 +0200

    chore: Add Next.js app router basic example

---
+++ packages/examples/next-app/src/app/page.tsx
@@ -0,0 +1,47 @@
+import type { SearchParams } from 'next/dist/server/request/search-params'
+import Image from 'next/image'
+import { Suspense } from 'react'
+import { Client } from './_components/client'
+import { Server } from './_components/server'
+
+type PageProps = {
+  searchParams: Promise<SearchParams>
+}
+
+export default async function Home({ searchParams }: PageProps) {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <Suspense>
+          <Server searchParams={searchParams} />
+        </Suspense>
+        <Suspense>
+          <Client />
+        </Suspense>
+        <a
+          className="w-full rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className="dark:invert"
+            src="/vercel.svg"
+            alt="Vercel logomark"
+            width={20}
+            height={20}
+          />
+          Deploy now
+        </a>
+      </main>
+    </div>
+  )
+}
