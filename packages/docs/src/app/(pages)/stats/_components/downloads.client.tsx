8a133ca36534ea9690ce6cad4f73a99ed26b63d2
François Best <github@francoisbest.com>
2025-08-14 22:23:40 +0200

    chore: Increase y axis width

--- packages/docs/src/app/(pages)/stats/_components/downloads.client.tsx
+++ packages/docs/src/app/(pages)/stats/_components/downloads.client.tsx
@@ -25,7 +25,7 @@ export function DownloadsGraph({
         curveType="monotone"
         index="date"
         tickGap={40}
-        yAxisWidth={40}
+        yAxisWidth={45}
         categories={dataKeys}
         colors={['red-500', 'zinc-500/50']}
         valueFormatter={v => formatStatNumber(v)}
