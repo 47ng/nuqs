872b2a9e7a14e642e5bce9b8128077729793d425
François Best <github@francoisbest.com>
2025-08-15 09:34:30 +0200

    chore: Improve stats page (#1067)

--- packages/docs/src/app/(pages)/stats/_components/downloads.tsx
+++ packages/docs/src/app/(pages)/stats/_components/downloads.tsx
@@ -45,6 +45,39 @@ export async function NPMDownloads() {
         title={
           <>
             <Download size={20} /> Last 90 days
+            <dl className="ml-auto flex gap-2">
+              <dt className="sr-only">combined</dt>
+              <dd>
+                {formatStatNumber(
+                  both.last90Days.reduce(
+                    (sum, { nuqs, ['next-usequerystate']: n_uqs }) =>
+                      sum + nuqs + n_uqs,
+                    0
+                  )
+                )}
+              </dd>
+              <span className="font-light text-zinc-500" aria-hidden>
+                |
+              </span>
+              <dt className="sr-only">nuqs</dt>
+              <dd className="text-red-500">
+                {formatStatNumber(
+                  nuqs.last90Days.reduce(
+                    (sum, { downloads }) => sum + downloads,
+                    0
+                  )
+                )}
+              </dd>
+              <dt className="sr-only">next-usequerystate</dt>
+              <dd className="text-zinc-500/50">
+                {formatStatNumber(
+                  nextUseQueryState.last90Days.reduce(
+                    (sum, { downloads }) => sum + downloads,
+                    0
+                  )
+                )}
+              </dd>
+            </dl>
           </>
         }
       />
@@ -54,6 +87,39 @@ export async function NPMDownloads() {
         title={
           <>
             <Download size={20} /> Last 30 days
+            <dl className="ml-auto flex gap-2">
+              <dt className="sr-only">combined</dt>
+              <dd>
+                {formatStatNumber(
+                  both.last30Days.reduce(
+                    (sum, { nuqs, ['next-usequerystate']: n_uqs }) =>
+                      sum + nuqs + n_uqs,
+                    0
+                  )
+                )}
+              </dd>
+              <span className="font-light text-zinc-500" aria-hidden>
+                |
+              </span>
+              <dt className="sr-only">nuqs</dt>
+              <dd className="text-red-500">
+                {formatStatNumber(
+                  nuqs.last30Days.reduce(
+                    (sum, { downloads }) => sum + downloads,
+                    0
+                  )
+                )}
+              </dd>
+              <dt className="sr-only">next-usequerystate</dt>
+              <dd className="text-zinc-500/50">
+                {formatStatNumber(
+                  nextUseQueryState.last30Days.reduce(
+                    (sum, { downloads }) => sum + downloads,
+                    0
+                  )
+                )}
+              </dd>
+            </dl>
           </>
         }
       />
