9124d1d29fffa15e9d78c8cea02b17ac18a2499b
François Best <github@francoisbest.com>
2025-08-20 00:16:32 +0200

    chore: Use RSC code block for Inertia adapter

--- packages/docs/content/docs/community-adapters/inertia-adapter-source.tsx
+++ packages/docs/content/docs/community-adapters/inertia-adapter-source.tsx
@@ -1,12 +1,16 @@
-import { DynamicCodeBlock } from 'fumadocs-ui/components/dynamic-codeblock';
+import { CodeBlock } from '@/src/components/code-block'
+import { SiTypescript } from '@icons-pack/react-simple-icons'
 
 export async function InertiaAdapterSource() {
-  let source = await fetch(
+  const source = await fetch(
     'https://raw.githubusercontent.com/47ng/nuqs-inertia-pingcrm/refs/heads/with-nuqs/resources/js/lib/nuqs-inertia-adapter.ts'
-  ).then((res) => res.text())
-  // todo: Add a PR on Fumadocs to support a title prop on DynamicCodeBlock
-  // or use the server component which should support a title.
-  // https://fumadocs.dev/docs/ui/components/dynamic-codeblock#server-component
-  source = `// resources/js/lib/nuqs-inertia-adapter.ts\n\n` + source.trim()
-  return <DynamicCodeBlock lang="ts" code={source} />
+  ).then(res => res.text())
+  return (
+    <CodeBlock
+      lang="ts"
+      icon={<SiTypescript size={14} />}
+      code={source.trim()}
+      title="resources/js/lib/nuqs-inertia-adapter.ts"
+    />
+  )
 }
