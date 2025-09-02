5c9b63b34ef506514e6078949369e9dfb2883428
François Best <github@francoisbest.com>
2025-09-02 12:01:48 +0200

    fix: Add QuerySpy to demos, use the Next.js adapter for reactivity

--- packages/docs/content/docs/options.client.tsx
+++ packages/docs/content/docs/options.client.tsx
@@ -1,15 +1,16 @@
 'use client'
 
+import { QuerySpy } from '@/src/components/query-spy'
 import { Button } from '@/src/components/ui/button'
 import { Checkbox } from '@/src/components/ui/checkbox'
 import { Label } from '@/src/components/ui/label'
 import { parseAsInteger, useQueryState } from 'nuqs'
-import { NuqsAdapter } from 'nuqs/adapters/react'
-import { useEffect, useState } from 'react'
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import { useState } from 'react'
 
 export function DemoSkeleton() {
   return (
-    <figure className="flex animate-pulse flex-wrap justify-around gap-2 rounded-md border border-dashed p-2">
+    <figure className="flex animate-pulse flex-wrap justify-around gap-2 rounded-xl border border-dashed p-1">
       <ComponentSkeleton />
       <ComponentSkeleton />
       <ComponentSkeleton />
@@ -26,15 +27,7 @@ function passThrough(search: URLSearchParams): URLSearchParams {
 }
 
 export function AlphabeticalSortDemo() {
-  const [hydrated, setHydrated] = useState(false)
   const [enableSorting, setEnableSorting] = useState(true)
-  useEffect(() => {
-    setHydrated(true)
-  }, [])
-
-  if (!hydrated) {
-    return <DemoSkeleton />
-  }
 
   return (
     <NuqsAdapter
@@ -48,7 +41,8 @@ export function AlphabeticalSortDemo() {
           />{' '}
           Enable alphabetical sorting on updates
         </Label>
-        <figure className="not-prose mt-4 mb-8 flex flex-wrap justify-around gap-2 rounded-md border border-dashed p-2">
+        <figure className="not-prose mt-4 mb-8 flex flex-wrap justify-around gap-2 rounded-xl border border-dashed p-1">
+          <QuerySpy keepKeys={['a', 'b', 'c']} />
           <ComponentToggle id="a" />
           <ComponentToggle id="b" />
           <ComponentToggle id="c" />
@@ -59,35 +53,27 @@ export function AlphabeticalSortDemo() {
 }
 
 export function TimestampDemo() {
-  const [hydrated, setHydrated] = useState(false)
-  useEffect(() => {
-    setHydrated(true)
-  }, [])
-
-  if (!hydrated) {
-    return <DemoSkeleton />
-  }
-
   return (
     <NuqsAdapter
       processUrlSearchParams={search => {
-        const params = new URLSearchParams(search)
-        params.set('ts', Date.now().toString())
-        return params
+        search.set('ts', Date.now().toString())
+        return search
       }}
     >
-      <figure className="flex flex-wrap justify-around gap-2 rounded-md border border-dashed p-2">
-        <ComponentIncrement id="d" />
-        <ComponentIncrement id="e" />
-        <ComponentIncrement id="f" />
-      </figure>
+      <>
+        <figure className="flex flex-wrap justify-around gap-2 rounded-xl border border-dashed p-1">
+          <QuerySpy keepKeys={['d', 'e', 'f', 'ts']} />
+          <ComponentIncrement id="d" />
+          <ComponentIncrement id="e" />
+          <ComponentIncrement id="f" />
+        </figure>
+      </>
     </NuqsAdapter>
   )
 }
 
 function ComponentIncrement({ id }: { id: string }) {
   const [count, setCount] = useQueryState(id, parseAsInteger.withDefault(0))
-
   return (
     <div className="rounded-xl p-1.5">
       <Button
@@ -102,7 +88,6 @@ function ComponentIncrement({ id }: { id: string }) {
 
 function ComponentToggle({ id }: { id: string }) {
   const [, setCount] = useQueryState(id, parseAsInteger.withDefault(0))
-
   return (
     <div className="rounded-xl p-1.5">
       <Button
