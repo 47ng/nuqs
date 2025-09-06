9c2cf91a5197efef963362c505ee726111282d3a
François Best <github@francoisbest.com>
2025-09-06 23:05:37 +0200

    chore: Disallow crawling the API

--- packages/docs/src/app/robots.ts
+++ packages/docs/src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/stats'
+      disallow: ['/stats', '/api']
     },
     sitemap: `${baseUrl}/sitemap.xml`
   }
