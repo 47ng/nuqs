7013a5af4615b03afdf1d00e346788bc91e1517c
TkDodo <office@dorfmeister.cc>
2025-09-25 12:26:20 +0200

    fix: NaN

--- packages/docs/content/docs/parsers/demos.tsx
+++ packages/docs/content/docs/parsers/demos.tsx
@@ -580,7 +580,7 @@ export function CustomMultiParserDemo() {
           className="border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 flex-1 rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
           value={filters.price?.gte ?? 0}
           onChange={e => {
-            setFilters(prev => ({...prev, price: { lte: prev.price?.lte ?? 0, gte: e.target.valueAsNumber }}))
+            setFilters(prev => ({...prev, price: { lte: prev.price?.lte ?? 0, gte: e.target.value === '' ? 0 : e.target.valueAsNumber }}))
           }}
           autoComplete="off"
         />
@@ -596,7 +596,7 @@ export function CustomMultiParserDemo() {
           className="border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 flex-1 rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
           value={filters.price?.lte ?? 0}
           onChange={e => {
-            setFilters(prev => ({...prev, price: { gte: prev.price?.gte ?? 0, lte: e.target.valueAsNumber }}))
+            setFilters(prev => ({...prev, price: { gte: prev.price?.gte ?? 0, lte: e.target.value === '' ? 0 : e.target.valueAsNumber }}))
           }}
           autoComplete="off"
         />
