122662ce255ebe2442787dddfade19498f323b41
TkDodo <office@dorfmeister.cc>
2025-09-29 12:35:14 +0200

    test: remove dynamic query state test

--- packages/e2e/next/cypress/e2e/repros.cy.ts
+++ packages/e2e/next/cypress/e2e/repros.cy.ts
@@ -113,21 +113,6 @@ describe('repro-758', () => {
 
 // --
 
-describe('repro-760', () => {
-  it('supports dynamic default values', () => {
-    cy.visit('/app/repro-760')
-    cy.contains('#hydration-marker', 'hydrated').should('be.hidden')
-    cy.get('#value-a').should('have.text', 'a')
-    cy.get('#value-b').should('have.text', 'b')
-    cy.get('#trigger-a').click()
-    cy.get('#trigger-b').click()
-    cy.get('#value-a').should('have.text', 'pass')
-    cy.get('#value-b').should('have.text', 'pass')
-  })
-})
-
-// --
-
 describe('repro-774', () => {
   it('updates internal state on navigation', () => {
     cy.visit('/app/repro-774')
