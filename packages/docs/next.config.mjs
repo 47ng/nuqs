e72714ad363ddab24495b6f9994f57317b390b33
François Best <github@francoisbest.com>
2025-10-12 15:33:35 +0200

    chore: fix env

--- packages/docs/next.config.mjs
+++ packages/docs/next.config.mjs
@@ -90,12 +90,14 @@ const sentryConfig = {
   widenClientFileUpload: true,
 
   release: {
-    setCommits: {
-      // https://github.com/getsentry/sentry-javascript-bundler-plugins/issues/443#issuecomment-1815988709
-      repo: '47ng/nuqs',
-      commit: process.env.VERCEL_GITHUB_COMMIT_SHA ?? 'HEAD',
-      previousCommit: process.env.VERCEL_GIT_PREVIOUS_SHA
-    }
+    setCommits: process.env.VERCEL_GIT_COMMIT_SHA
+      ? {
+          // https://github.com/getsentry/sentry-javascript-bundler-plugins/issues/443#issuecomment-1815988709
+          repo: '47ng/nuqs',
+          commit: process.env.VERCEL_GIT_COMMIT_SHA,
+          previousCommit: process.env.VERCEL_GIT_PREVIOUS_SHA
+        }
+      : { auto: true }
   },
 
   // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers. (increases server load)
