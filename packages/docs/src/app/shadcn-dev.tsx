d384e7b26b6d5484f151281097eba364975b8f67
François Best <github@francoisbest.com>
2025-12-14 14:01:45 +0100

    test: add links to local dev env

---
+++ packages/docs/src/app/shadcn-dev.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <ul>
+      <li>
+        <a href="http://192.168.1.200:4000/create">Using IP address</a>
+      </li>
+      <li>
+        <a href="http://m4x.local:4000/create">Using mDNS</a>
+      </li>
+    </ul>
+  )
+}
