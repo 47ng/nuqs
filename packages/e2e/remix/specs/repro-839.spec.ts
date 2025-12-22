2c9eaafd870c04b50dee68ea4f1ceed723f19e69
François Best <github@francoisbest.com>
2025-12-22 11:01:18 +0100

    test: add Remix tests

---
+++ packages/e2e/remix/specs/repro-839.spec.ts
@@ -0,0 +1,5 @@
+import { testRepro839LocationStatePersistence } from 'e2e-shared/specs/react-router/repro-839-location-state-persistence.spec.ts'
+
+testRepro839LocationStatePersistence({
+  path: '/repro-839'
+})
