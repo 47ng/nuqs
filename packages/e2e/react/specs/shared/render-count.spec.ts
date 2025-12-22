9d10c572c0b4f2caf2c13ebd13c372092ab748ba
François Best <github@francoisbest.com>
2025-12-22 09:34:10 +0100

    test: disable render count checking on React SPA + FPN

--- packages/e2e/react/specs/shared/render-count.spec.ts
+++ packages/e2e/react/specs/shared/render-count.spec.ts
@@ -4,23 +4,26 @@ const hooks = ['useQueryState', 'useQueryStates'] as const
 const shallows = [true, false] as const
 const histories = ['replace', 'push'] as const
 
-for (const hook of hooks) {
-  for (const shallow of shallows) {
-    for (const history of histories) {
-      for (const startTransition of [false, true]) {
-        testRenderCount({
-          path: `/render-count/${hook}/${shallow}/${history}/${startTransition}`,
-          hook,
-          props: {
-            shallow,
-            history,
-            startTransition
-          },
-          expected: {
-            mount: 1,
-            update: 2
-          }
-        })
+// FPN makes render count checking unreliable
+if (process.env.FULL_PAGE_NAV_ON_SHALLOW_FALSE !== 'true') {
+  for (const hook of hooks) {
+    for (const shallow of shallows) {
+      for (const history of histories) {
+        for (const startTransition of [false, true]) {
+          testRenderCount({
+            path: `/render-count/${hook}/${shallow}/${history}/${startTransition}`,
+            hook,
+            props: {
+              shallow,
+              history,
+              startTransition
+            },
+            expected: {
+              mount: 1,
+              update: 2
+            }
+          })
+        }
       }
     }
   }
