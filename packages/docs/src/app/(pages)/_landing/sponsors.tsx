79deef1a0b4ba199ca23e11ea7b277d55452f3e3
François Best <github@francoisbest.com>
2025-12-24 20:50:33 +0100

    doc: add shadcnstudio as a featured sponsor

--- packages/docs/src/app/(pages)/_landing/sponsors.tsx
+++ packages/docs/src/app/(pages)/_landing/sponsors.tsx
@@ -263,7 +263,14 @@ export function AsideSponsors() {
           Sponsored by
         </h3>
       </a>
-      <NextJSWeeklyAsideSponsor />
+      <ul className="space-y-2">
+        <li>
+          <NextJSWeeklyAsideSponsor />
+        </li>
+        <li>
+          <ShadcnStudioAsideSponsor />
+        </li>
+      </ul>
     </aside>
   )
 }
@@ -296,6 +303,92 @@ export function NextJSWeeklyAsideSponsor() {
         <p className="text-sm">Stay up to date on Next.js</p>
         <p className="text-muted-foreground text-xs">
           A weekly newsletter to keep up with what's happening in the ecosystem.
+        </p>
+      </section>
+    </a>
+  )
+}
+
+export function ShadcnStudioAsideSponsor() {
+  return (
+    <a
+      href="https://shadcnstudio.com/?utm_source=nuqs&utm_medium=banner&utm_campaign=github"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="group"
+    >
+      <section className="text-muted-foreground space-y-4 rounded-md border border-dashed px-4 py-6 transition-colors group-hover:text-current group-active:text-current">
+        <header className="mx-auto flex items-center justify-center gap-2">
+          <svg
+            viewBox="0 0 328 329"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="size-8"
+            aria-hidden
+          >
+            <rect
+              y="0.5"
+              width="328"
+              height="328"
+              rx="164"
+              fill="currentColor"
+            />
+            <path
+              d="M165.018 72.3008V132.771C165.018 152.653 148.9 168.771 129.018 168.771H70.2288"
+              strokeWidth="20"
+              className="stroke-background"
+            />
+            <path
+              d="M166.627 265.241L166.627 204.771C166.627 184.889 182.744 168.771 202.627 168.771L261.416 168.771"
+              strokeWidth="20"
+              className="stroke-background"
+            />
+            <line
+              x1="238.136"
+              y1="98.8184"
+              x2="196.76"
+              y2="139.707"
+              strokeWidth="20"
+              className="stroke-background"
+            />
+            <line
+              x1="135.688"
+              y1="200.957"
+              x2="94.3128"
+              y2="241.845"
+              strokeWidth="20"
+              className="stroke-background"
+            />
+            <line
+              x1="133.689"
+              y1="137.524"
+              x2="92.5566"
+              y2="96.3914"
+              strokeWidth="20"
+              className="stroke-background"
+            />
+            <line
+              x1="237.679"
+              y1="241.803"
+              x2="196.547"
+              y2="200.671"
+              strokeWidth="20"
+              className="stroke-background"
+            />
+          </svg>
+          <div className="flex flex-col">
+            <span className="text-sm leading-tight font-medium">
+              shadcnstudio.com
+            </span>
+            <span className="text-muted-foreground text-xs">
+              shadcn blocks & templates
+            </span>
+          </div>
+        </header>
+        <p className="text-muted-foreground text-center text-xs">
+          Accelerate your project development with ready-to-use, and fully
+          customizable shadcn ui Components, Blocks, UI Kits, Boilerplates,
+          Templates and Themes with AI Tools 🪄.
         </p>
       </section>
     </a>
