bec99fd83af8e0607b10366282364f00a7a4ff94
François Best <github@francoisbest.com>
2026-01-31 17:05:26 +0100

    test: add nuqs state update in repro-1293 to showcase issue 1156

--- packages/e2e/shared/specs/repro-1293.spec.ts
+++ packages/e2e/shared/specs/repro-1293.spec.ts
@@ -41,4 +41,66 @@ export const testRepro1293 = defineTest('repro-1293', ({ path }) => {
       'should not re-render Page B when navigating back to Page A'
     )
   })
+
+  it('should not pre-render the target page with source search params when navigating to another page after a nuqs state update', async ({
+    page
+  }) => {
+    using logSpy = setupLogSpy(page)
+    // Navigate to Page A
+    await navigateTo(page, path + '/a')
+
+    // Increment count to 1
+    logSpy.logs.length = 0 // Clear logs
+    await page.getByRole('button', { name: 'Increment' }).click()
+    await expect(page).toHaveURL(
+      url =>
+        url.pathname.endsWith(`${path}/a`) && // support basePath
+        url.searchParams.get('count') === '1'
+    )
+
+    // Navigate to Page B
+    logSpy.logs.length = 0 // Clear logs
+    await page.getByRole('link', { name: 'Go to Page B' }).click()
+    await expect(page).toHaveURL(
+      url =>
+        url.pathname.endsWith(`${path}/b`) && // support basePath
+        url.searchParams.get('count') === '2'
+    )
+    await assertLogCount(
+      logSpy,
+      'a: 2',
+      0,
+      'should not re-render Page A when navigating to Page B after a nuqs state update'
+    )
+    await assertLogCount(
+      logSpy,
+      'b: 1',
+      0,
+      'should not pre-render Page B with pre-navigation search params'
+    )
+
+    // Increment on page B
+    logSpy.logs.length = 0 // Clear logs
+    await page.getByRole('button', { name: 'Increment' }).click()
+    await expect(page).toHaveURL(
+      url =>
+        url.pathname.endsWith(`${path}/b`) && // support basePath
+        url.searchParams.get('count') === '3'
+    )
+
+    // Go back to Page A
+    logSpy.logs.length = 0 // Clear logs
+    await page.getByRole('button', { name: 'Go back' }).click()
+    await expect(page).toHaveURL(
+      url =>
+        url.pathname.endsWith(`${path}/a`) && // support basePath
+        url.searchParams.get('count') === '1'
+    )
+    await assertLogCount(
+      logSpy,
+      'a: 3',
+      0,
+      'should not pre-render Page A when navigating back after a nuqs state update'
+    )
+  })
 })
--- packages/e2e/shared/specs/repro-1293.tsx
+++ packages/e2e/shared/specs/repro-1293.tsx
@@ -11,18 +11,32 @@ const searchParams = {
   count: parseAsInteger.withDefault(0)
 }
 const serialize = createSerializer(searchParams)
-const useCount = () => useQueryStates(searchParams)[0].count
 
 export function Repro1293PageA({ linkHref }: Props) {
-  const count = useCount()
+  const [{ count }, setSearchParams] = useQueryStates(searchParams)
   console.log(`a: ${count}`)
   const Link = useLink()
-  const href = serialize(linkHref, { count: 1 })
-  return <Link href={href}>Go to Page B</Link>
+  const href = serialize(linkHref, { count: count + 1 })
+  return (
+    <>
+      <button onClick={() => setSearchParams({ count: count + 1 })}>
+        Increment
+      </button>
+
+      <Link href={href}>Go to Page B</Link>
+    </>
+  )
 }
 
 export function Repro1293PageB() {
-  const count = useCount()
+  const [{ count }, setSearchParams] = useQueryStates(searchParams)
   console.log(`b: ${count}`)
-  return <button onClick={() => history.back()}>Go back</button>
+  return (
+    <>
+      <button onClick={() => setSearchParams({ count: count + 1 })}>
+        Increment
+      </button>
+      <button onClick={() => history.back()}>Go back</button>
+    </>
+  )
 }
