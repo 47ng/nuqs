0488d769aef6a5309a05ebbcea3f42c5732951a5
François Best <github@francoisbest.com>
2026-03-10 14:32:08 +0100

    doc: add 1771 Technologies as a sponsor (#1357)

--- packages/docs/src/app/(pages)/_landing/sponsors.tsx
+++ packages/docs/src/app/(pages)/_landing/sponsors.tsx
@@ -33,6 +33,12 @@ const SPONSORS: Sponsors = [
     img: 'https://avatars.githubusercontent.com/u/130389858?s=200&v=4'
   },
   {
+    handle: '1771-Technologies',
+    name: '1771 Technologies',
+    url: 'https://1771technologies.com/?utm_source=nuqs&utm_medium=sponsor&utm_campaign=nuqs',
+    img: 'https://avatars.githubusercontent.com/u/148620833?s=200&v=4'
+  },
+  {
     handle: 'unkey.com',
     name: 'Unkey',
     url: 'https://unkey.com',
