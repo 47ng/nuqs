116d8d127e198ea26c5f3072587c9a66a627d78c
François Best <github@francoisbest.com>
2026-07-18 21:54:03 +0200

    Revert "chore: throwaway commit to test bundle size workflow"
    This reverts commit c0c32574b883ea4b046f8214ace53ffd887a8606.

--- packages/nuqs/src/index.ts
+++ packages/nuqs/src/index.ts
@@ -25,6 +25,3 @@ export {
 } from './standard-schema'
 export * from './useQueryState'
 export * from './useQueryStates'
-
-export const __bundleSizeCanary =
-  'q7Xv9kM2pLzR4wTb8nJdY3fGh6sCa1eWi5uHo0NgKrPmZxQtSjVyBl'
