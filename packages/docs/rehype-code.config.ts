ca6bd2f137e21f2195eae5d9c5d7d0ed0caff387
François Best <github@francoisbest.com>
2026-08-21 09:01:00 +0200

    doc: remove italics in syntax highlighting

--- packages/docs/rehype-code.config.ts
+++ packages/docs/rehype-code.config.ts
@@ -1,4 +1,23 @@
 import type { RehypeCodeOptions } from 'fumadocs-core/mdx-plugins'
+import type { ShikiTransformer } from 'shiki'
+
+const withoutItalics: ShikiTransformer = {
+  name: 'without-italics',
+  tokens(lines) {
+    for (const line of lines) {
+      for (const token of line) {
+        if (typeof token.htmlStyle !== 'object') {
+          continue
+        }
+        for (const key of Object.keys(token.htmlStyle)) {
+          if (key.endsWith('font-style')) {
+            delete token.htmlStyle[key]
+          }
+        }
+      }
+    }
+  }
+}
 
 export const rehypeCodeOptions: RehypeCodeOptions = {
   themes: {
@@ -6,5 +25,6 @@ export const rehypeCodeOptions: RehypeCodeOptions = {
     dark: 'catppuccin-mocha'
   },
   inline: 'tailing-curly-colon',
-  defaultColor: false
+  defaultColor: false,
+  transformers: [withoutItalics]
 }
